fd7ce03ca7cc6e137f6dc5bc04949edc402bba8d
jonfodi <masterfodi@gmail.com>
2022-04-26 00:30:30 -0400

    create compile env for firmware

--- dev_container/compile_docker.sh
+++ dev_container/compile_docker.sh
@@ -14,12 +14,14 @@ COMPONENT=""
 # build base ubuntu image  
 docker build -t ${IMAGE_NAME} .
 
-
+# start and mount program files to container workdir 
 docker run -t -d -v ${C_CODE}:${WORKDIR} --name ${CONTAINER_NAME} ${IMAGE_NAME}
 
 # install requirements 
 docker exec ${CONTAINER_NAME} pip3 install -r /usr/project/requirements.txt
 
+# open containers shell
+docker exec -it ${CONTAINER_NAME} bash
 
 # add wrapper feature?
 #docker exec ${CONTAINER_NAME} scons --target=${COMPONENT}
