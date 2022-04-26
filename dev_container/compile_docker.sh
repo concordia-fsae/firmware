9dec05d40ceeed1b9bd01739cb436cb4f3f95298
jonfodi <masterfodi@gmail.com>
2022-04-25 23:14:02 -0400

    ubuntu container to compile with scons

---
+++ dev_container/compile_docker.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# TODO: add installation and starting of docker daemon if necessary
+
+IMAGE_NAME="image_scons"
+CONTAINER_NAME="container_scons"
+
+C_CODE=/Users/garyfodi/Desktop/PROJECTS/SAE/DAQ/firmware/
+WORKDIR=/usr/project
+
+
+COMPONENT=""
+
+# build base ubuntu image  
+docker build -t ${IMAGE_NAME} .
+
+
+docker run -t -d -v ${C_CODE}:${WORKDIR} --name ${CONTAINER_NAME} ${IMAGE_NAME}
+
+# install requirements 
+docker exec ${CONTAINER_NAME} pip3 install -r /usr/project/requirements.txt
+
+
+# add wrapper feature?
+#docker exec ${CONTAINER_NAME} scons --target=${COMPONENT}
