41feb297782064365352eea90ad0bdb74aee42ab
Ricky Lopez <rickyerlopez@gmail.com>
2023-08-28 21:27:22 -0700

    Update arm-none and st-flash
    - arm-none-eabi:
      - separate bin file generation from elf file generation
      - add ability to generate asm files
    - st-flash:
      - add ability to flash to a specific address, or default if not specified

--- site_scons/site_tools/st-flash.py
+++ site_scons/site_tools/st-flash.py
@@ -1,19 +1,32 @@
+import SCons
 from SCons.Script import *
 
 
-# def _flash_chip(env, bin_file):
-#     env["bin_file"] = bin_file
-#     return env.flash_chip(bin_file)
-
-
 def generate(env):
-    # env.AddMethod(_flash_chip, "flash")
+    if not GetOption("verbose"):
+        env["FLASHCOMSTR"] = "Flashing $SOURCE to address ${hex(addr)}"
+        env["ERASECOMSTR"] = "Erasing chip"
 
-    env['BUILDERS']['flash'] = SCons.Builder.Builder(
-        action = SCons.Action.Action(
-            "st-flash write $SOURCE 0x08000000"
+    env["BUILDERS"]["_erase"] = SCons.Builder.Builder(
+        action=SCons.Action.Action(
+            "st-flash erase",
+            "$ERASECOMSTR",
         )
     )
+
+    env["BUILDERS"]["_flash"] = SCons.Builder.Builder(
+        action=SCons.Action.Action(
+            "st-flash write $SOURCE $addr",
+            "$FLASHCOMSTR",
+        )
+    )
+
+    env.AddMethod(flash, "flash")
+
+
+def flash(env, source, target=0x08000000):
+    env._erase()
+    return env._flash(source=source, addr=target)
 
 
 def exists():
