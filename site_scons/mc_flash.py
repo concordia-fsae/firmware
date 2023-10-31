22155cca228a31d85a0372337422d227cbf28776
Joshua Lafleur <77811114+JoshLafleur@users.noreply.github.com>
2023-10-31 16:22:36 -0400

    Added Hexadecimal and Decimal support for user inputs

--- site_scons/mc_flash.py
+++ site_scons/mc_flash.py
@@ -33,7 +33,7 @@ def set_eeprom(can, id, address, data):
     msg_resp += str(id[1])
     msg_resp += str(int(id[2]) + 1)
     print(msg_resp)
-    
+
     can.flush()
     candapter_driver.send_message(can, id, 8, message)
 
@@ -51,6 +51,37 @@ def set_eeprom(can, id, address, data):
     else:
         print("ERROR writing to EEPROM!")
         return False
+
+
+def checkHex(usr_in):
+    if (usr_in[0] + usr_in[1] == "0x"):
+        return True
+    return False
+
+
+def convertToHex(usr_in, big_endian, length=0):
+    inp = ""
+    output = ""
+
+    if checkHex(usr_in):
+        inp = usr_in[2:]
+    else:
+        inp = hex(int(usr_in))[2:]
+
+    if len(inp) < length:
+        for x in range(length-len(inp)):
+            inp = "0"+inp
+
+    if not big_endian:
+        if len(inp) % 2 == 1:
+            inp = "0" + inp
+        for x in range(0, len(inp), 2):
+            output = output + inp[len(inp) - 1 - x - 1]
+            output = output + inp[len(inp) - x - 1]
+    else:
+        output = inp
+
+    return output
 
 
 def main():
@@ -71,8 +102,8 @@ def main():
             if usr_in == 1:
                 done = True
             elif usr_in == 2:
-                id = input("Motor Controller EEPROM command ID, 3 chars: (hex little-endian, no 0x) ")
-                address = input("EEPROM CAN address, 4 chars: (hex little-endian, no 0x) ")
+                id = convertToHex(input("Motor Controller EEPROM command ID: "), True, 3)
+                address = convertToHex(input("EEPROM CAN address: "), False, 4)
                 get_eeprom(can, id, address)
             elif usr_in == 3:
                 id = input("Motor Controller EEPROM command ID, 3 chars: (hex little-endian, no 0x) ")
