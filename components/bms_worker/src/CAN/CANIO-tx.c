84dbb582d3b43e940908a3094baf829834f06691
Joshua Lafleur <77811114+JoshLafleur@users.noreply.github.com>
2024-03-25 18:14:44 -0400

    test

--- components/bms_worker/src/CAN/CANIO-tx.c
+++ components/bms_worker/src/CAN/CANIO-tx.c
@@ -18,6 +18,7 @@
 #include "string.h"
 
 // imports for data access
+#include "BatteryMonitoring.h"
 #include "IO.h"
 
 
@@ -37,11 +38,11 @@ typedef struct
  *                              D E F I N E S
  ******************************************************************************/
 
-#define MSG_packTable_100Hz_SIZE    (sizeof(MSG_packTable_100Hz) / sizeof(packTable_S))
-#define MSG_packTable_10Hz_SIZE     (sizeof(MSG_packTable_10Hz) / sizeof(packTable_S))
-#define MSG_packTable_1Hz_SIZE      (sizeof(MSG_packTable_1Hz) / sizeof(packTable_S))
+#define MSG_packTable_100Hz_SIZE (sizeof(MSG_packTable_100Hz) / sizeof(packTable_S))
+#define MSG_packTable_10Hz_SIZE  (sizeof(MSG_packTable_10Hz) / sizeof(packTable_S))
+#define MSG_packTable_1Hz_SIZE   (sizeof(MSG_packTable_1Hz) / sizeof(packTable_S))
 
-#define MSG_UID_SEGMENT(id)         (id + IO.addr)
+#define MSG_UID_SEGMENT(id) (id + IO.addr)
 
 
 /******************************************************************************
@@ -54,24 +55,28 @@ static cantx_S cantx;
  *          P R I V A T E  F U N C T I O N  P R O T O T Y P E S
  ******************************************************************************/
 
-static const packTable_S* packNextMessage(const packTable_S *packTable,
-                                          const uint8_t     packTableLength,
-                                          uint8_t           *index,
-                                          CAN_data_T        *message,
-                                          uint8_t           *nextCounter);
+static const packTable_S* packNextMessage(const packTable_S* packTable,
+                                          const uint8_t      packTableLength,
+                                          uint8_t*           index,
+                                          CAN_data_T*        message,
+                                          uint8_t*           nextCounter);
 
 static bool MSG_pack_BMS_100Hz(CAN_data_T* message, const uint8_t counter);
-static bool MSG_pack_BMS_10Hz(CAN_data_T* message, const uint8_t counter);
-static bool MSG_pack_BMS_1Hz(CAN_data_T* message, const uint8_t counter);
+static bool MSG_pack_BMS_100Hz1(CAN_data_T* message, const uint8_t counter);
+//static bool MSG_pack_BMS_10Hz(CAN_data_T* message, const uint8_t counter);
+//static bool MSG_pack_BMS_1Hz(CAN_data_T* message, const uint8_t counter);
 
 static const packTable_S MSG_packTable_100Hz[] = {
+    //{ &MSG_pack_BMS_100Hz, 0x100, 8U },
+    //{ &MSG_pack_BMS_100Hz1, 0x110, 8U },
+};
+static const packTable_S MSG_packTable_10Hz[] = {
+    //{ &MSG_pack_BMS_10Hz, 0x10, 8U },
+};
+static const packTable_S MSG_packTable_1Hz[] = {
+    //{ &MSG_pack_BMS_1Hz, 0x1, 8U },
     { &MSG_pack_BMS_100Hz, 0x100, 8U },
-};
-static const packTable_S MSG_packTable_10Hz[]  = {
-    { &MSG_pack_BMS_10Hz, 0x10, 8U },
-};
-static const packTable_S MSG_packTable_1Hz[]   = {
-    { &MSG_pack_BMS_1Hz, 0x1, 8U },
+    { &MSG_pack_BMS_100Hz1, 0x110, 8U },
 };
 
 
@@ -91,14 +96,14 @@ void CANTX_BUS_A_100Hz_SWI(void)
         return;
     }
 
-    static uint8_t    counter = 0U;
-    CAN_data_T        message;
+    static uint8_t counter = 0U;
+    CAN_data_T     message;
 
-    const packTable_S *entry  = packNextMessage((const packTable_S*)&MSG_packTable_100Hz,
-                                                MSG_packTable_100Hz_SIZE,
-                                                &cantx.tx_100Hz_msg,
-                                                &message,
-                                                &counter);
+    const packTable_S* entry = packNextMessage((const packTable_S*)&MSG_packTable_100Hz,
+                                               MSG_packTable_100Hz_SIZE,
+                                               &cantx.tx_100Hz_msg,
+                                               &message,
+                                               &counter);
 
     if (entry != NULL)
     {
@@ -115,14 +120,14 @@ void CANTX_BUS_A_10Hz_SWI(void)
         return;
     }
 
-    static uint8_t    counter = 0U;
-    CAN_data_T        message;
+    static uint8_t counter = 0U;
+    CAN_data_T     message;
 
-    const packTable_S *entry  = packNextMessage((const packTable_S*)&MSG_packTable_10Hz,
-                                                MSG_packTable_10Hz_SIZE,
-                                                &cantx.tx_10Hz_msg,
-                                                &message,
-                                                &counter);
+    const packTable_S* entry = packNextMessage((const packTable_S*)&MSG_packTable_10Hz,
+                                               MSG_packTable_10Hz_SIZE,
+                                               &cantx.tx_10Hz_msg,
+                                               &message,
+                                               &counter);
 
     if (entry != NULL)
     {
@@ -139,14 +144,14 @@ void CANTX_BUS_A_1Hz_SWI(void)
         return;
     }
 
-    static uint8_t    counter = 0U;
-    CAN_data_T        message;
+    static uint8_t counter = 0U;
+    CAN_data_T     message;
 
-    const packTable_S *entry  = packNextMessage((const packTable_S*)&MSG_packTable_1Hz,
-                                                MSG_packTable_1Hz_SIZE,
-                                                &cantx.tx_1Hz_msg,
-                                                &message,
-                                                &counter);
+    const packTable_S* entry = packNextMessage((const packTable_S*)&MSG_packTable_1Hz,
+                                               MSG_packTable_1Hz_SIZE,
+                                               &cantx.tx_1Hz_msg,
+                                               &message,
+                                               &counter);
 
     if (entry != NULL)
     {
@@ -159,16 +164,16 @@ void CANTX_BUS_A_1Hz_SWI(void)
  *                     P R I V A T E  F U N C T I O N S
  ******************************************************************************/
 
-static const packTable_S* packNextMessage(const packTable_S *packTable,
-                                          const uint8_t     packTableLength,
-                                          uint8_t           *index,
-                                          CAN_data_T        *message,
-                                          uint8_t           *nextCounter)
+static const packTable_S* packNextMessage(const packTable_S* packTable,
+                                          const uint8_t      packTableLength,
+                                          uint8_t*           index,
+                                          CAN_data_T*        message,
+                                          uint8_t*           nextCounter)
 {
     while (*index < packTableLength)
     {
-        const packTable_S *entry  = &packTable[(*index)++];
-        uint16_t          counter = *nextCounter;
+        const packTable_S* entry   = &packTable[(*index)++];
+        uint16_t           counter = *nextCounter;
         if (*index == packTableLength)
         {
             (*nextCounter)++;
@@ -184,23 +189,41 @@ static const packTable_S* packNextMessage(const packTable_S *packTable,
 
 static bool MSG_pack_BMS_100Hz(CAN_data_T* message, const uint8_t counter)
 {
-    message->u64    = 0x100;
-    message->u32[1] = counter;
+    UNUSED(counter);
+    message->u16[0] = BMS.voltage.min;
+    message->u16[1] = BMS.voltage.max;
+    message->u16[2] = BMS.voltage.avg;
     return true;
 }
 
-static bool MSG_pack_BMS_10Hz(CAN_data_T* message, const uint8_t counter)
+static bool MSG_pack_BMS_100Hz1(CAN_data_T* message, const uint8_t counter)
 {
-    message->u64    = 0x10;
-    message->u32[1] = counter;
+    UNUSED(counter);
+    message->u16[0] = BMS.relativeSoC.min;
+    message->u16[1] = BMS.relativeSoC.max;
+    message->u16[2] = BMS.relativeSoC.avg;
     return true;
 }
 
-static bool MSG_pack_BMS_1Hz(CAN_data_T* message, const uint8_t counter)
+//static bool MSG_pack_BMS_10Hz(CAN_data_T* message, const uint8_t counter)
+//{
+//    message->u64    = 0x10;
+//    message->u32[1] = counter;
+//    return true;
+//}
+//
+//static bool MSG_pack_BMS_1Hz(CAN_data_T* message, const uint8_t counter)
+//{
+//    message->u64    = 0x1;
+//    message->u32[1] = counter;
+//    return true;
+//}
+
+static void CANIO_tx_1kHz_PRD(void)
 {
-    message->u64    = 0x1;
-    message->u32[1] = counter;
-    return true;
+    // transmit 100Hz messages
+    cantx.tx_100Hz_msg = 0U;
+    SWI_invoke(CANTX_BUS_A_100Hz_swi);
 }
 
 /**
@@ -220,16 +243,14 @@ static void CANIO_tx_100Hz_PRD(void)
  */
 static void CANIO_tx_10Hz_PRD(void)
 {
-    // transmit 10Hz messages
-    // cantx.tx_10Hz_msg = 0U;
-    // SWI_invoke(CANTX_BUS_A_10Hz_swi);
+    cantx.tx_10Hz_msg = 0U;
+    SWI_invoke(CANTX_BUS_A_10Hz_swi);
 }
 
 static void CANIO_tx_1Hz_PRD(void)
 {
-    // transmit 1Hz messages
-    // cantx.tx_1Hz_msg = 0U;
-    // SWI_invoke(CANTX_BUS_A_1Hz_swi);
+    cantx.tx_1Hz_msg = 0U;
+    SWI_invoke(CANTX_BUS_A_1Hz_swi);
 }
 
 /**
@@ -245,6 +266,7 @@ static void CANIO_tx_init(void)
 
 const ModuleDesc_S CANIO_tx = {
     .moduleInit        = &CANIO_tx_init,
+    .periodic1kHz_CLK = &CANIO_tx_1kHz_PRD,
     .periodic100Hz_CLK = &CANIO_tx_100Hz_PRD,
     .periodic10Hz_CLK  = &CANIO_tx_10Hz_PRD,
     .periodic1Hz_CLK   = &CANIO_tx_1Hz_PRD,
