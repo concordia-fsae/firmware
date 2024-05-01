69ea52f6d1db797ea9d41c961fce3523cb4c2969
Ricky Lopez <rickyerlopez@gmail.com>
2024-04-30 21:55:56 -0700

    testing routine control and cmdqueue

--- conUDS/src/main.rs
+++ conUDS/src/main.rs
@@ -1,12 +1,17 @@
 use core::time;
+// use futures::future::BoxFuture;
 use std::{sync::Arc, sync::Mutex};
-use futures::future::BoxFuture;
+use std::io::stdin;
 
 use anyhow::Result;
+use anyhow::anyhow;
+use ecu_diagnostics::dynamic_diag::{DiagProtocol, DiagPayload};
+use ecu_diagnostics::uds::{UDSProtocol, UdsCommand};
+use modules::canio::CANIO;
+use tokio::sync::mpsc;
+use tokio::sync::oneshot;
 
 pub mod modules;
-
-use crate::modules::canio;
 
 struct App {
     exit: bool,
@@ -14,62 +19,149 @@ struct App {
 
 impl App {
     fn new() -> Self {
-        Self {
-            exit: false,
+        Self { exit: false }
+    }
+}
+
+// struct CmdResp {
+//     cmd: Vec<u8>,
+//     resp: oneshot::Sender<Vec<u8>>,
+// }
+
+// impl CmdResp {
+//     async fn send(cmd: Vec<u8>, send: &mut mpsc::Sender<Self>) -> oneshot::Receiver<Vec<u8>> {
+//         let (resp, recv) = oneshot::channel::<Vec<u8>>();
+
+//         let _ = send.send(Self { cmd, resp }).await;
+//         recv
+//     }
+// }
+
+#[derive(Debug)]
+pub enum CanioCmd {
+    UdsCmdNoResponse(Vec<u8>),
+    UdsCmdWithResponse(Vec<u8>, oneshot::Sender<Vec<u8>>),
+}
+
+impl CanioCmd {
+    #[allow(dead_code)]
+    async fn send_with_resp(
+        buf: &[u8],
+        queue: mpsc::Sender<CanioCmd>,
+    ) -> Result<oneshot::Receiver<Vec<u8>>> {
+        let (tx, rx) = oneshot::channel();
+        match queue.send(Self::UdsCmdWithResponse(buf.to_owned(), tx)).await {
+            Ok(_) => Ok(rx),
+            Err(e) => Err(anyhow!(e)),
         }
     }
 }
 
-type TskFn = fn(Arc<Mutex<App>>) -> BoxFuture<'static, Result<()>>;
-
+enum PrdCmd {
+    PersistentTesterPresent(bool),
+}
 
 #[tokio::main]
 async fn main() {
+    println!("startup");
 
     let app = Arc::new(Mutex::new(App::new()));
-    let mut handles = vec![];
+    let app_canio = Arc::clone(&app);
+    let app_10ms = Arc::clone(&app);
 
-    let tasks: Vec<TskFn> = vec![
-        |app| Box::pin(tsk_canio(app)),
-        |app| Box::pin(tsk_10ms(app))];
-    for tsk in tasks {
-        let app_clone = Arc::clone(&app);
-        handles.push(tokio::spawn(async move {tsk(app_clone)}));
+    let (uds_queue_tx, uds_queue_rx) = mpsc::channel::<CanioCmd>(100);
+    let uds_queue_tx2 = uds_queue_tx.clone();
+    let (cmd_queue_tx, mut cmd_queue_rx) = mpsc::channel::<PrdCmd>(10);
+
+    let t1 = tokio::spawn(async move { tsk_canio(app_canio, uds_queue_rx).await });
+    let t2 = tokio::spawn(async move { tsk_10ms(app_10ms, &mut cmd_queue_rx, uds_queue_tx2).await });
+
+    let _ = cmd_queue_tx
+        .send(PrdCmd::PersistentTesterPresent(true))
+        .await;
+
+    let mut buf = String::new();
+    while !stdin().read_line(&mut buf).is_ok() {
+        // wait for user to hit enter
     }
+
+    let buf = DiagPayload::new(
+        UdsCommand::RoutineControl.into(),
+        &[0x01, 0xf0, 0x0f],
+    ).to_bytes();
+
+    let resp = CanioCmd::send_with_resp(&buf, uds_queue_tx);
+
+    if let Ok(resp) = resp.await {
+        if let Ok(resp) = resp.await {
+            println!("response: {:?}", resp);
+        }
+
+    }
+
+    let _ = t1.await.unwrap();
+    let _ = t2.await.unwrap();
 
     // fixme: probably wait for shutdown signal here, then signal threads to exit
 
-    for handle in handles {
-        let res = handle.await.unwrap().await;
-        if !res.is_ok() {
-            println!("{:?}", res);
-            app.lock().unwrap().exit = true;
-        }
-    }
+    // for handle in handles {
+    //     let res = handle.await.unwrap().await;
+    //     if !res.is_ok() {
+    //         println!("{:?}", res);
+    //         app.lock().unwrap().exit = true;
+    //     }
+    // }
 }
-
 
 /// CANIO task
 ///
 /// Will handle converting between UDS commands and CAN frames, and interfacing with the hardware
-async fn tsk_canio(app: Arc<Mutex<App>>) -> Result<()> {
-    canio::process()?;
+async fn tsk_canio(app: Arc<Mutex<App>>, mut uds_queue: mpsc::Receiver<CanioCmd>) -> Result<()> {
+    // init
+    let mut canio = CANIO::new(0x456, 0x123, &mut uds_queue)?;
+
+    // loop
+    while !app.try_lock().is_ok_and(|app| app.exit) {
+        canio.process().await?;
+    }
+
     Ok(())
 }
 
 /// 10ms(100Hz) periodic task
 ///
 /// This task will run at 100Hz in order to handle periodic actions
-async fn tsk_10ms(app: Arc<Mutex<App>>) -> Result<()>{
-    loop {
-        if app.lock().unwrap().exit {
-            break;
+async fn tsk_10ms(
+    app: Arc<Mutex<App>>,
+    cmd_queue: &mut mpsc::Receiver<PrdCmd>,
+    uds_queue: mpsc::Sender<CanioCmd>,
+) -> Result<()> {
+    let mut send_tp = false;
+
+    while !app.try_lock().is_ok_and(|app| app.exit) {
+        if let Ok(cmd) = cmd_queue.try_recv() {
+            match cmd {
+                PrdCmd::PersistentTesterPresent(state) => {
+                    if state {
+                        println!("Enabling persisting tp");
+                    } else {
+                        println!("Disabling persisting tp");
+                    }
+                    send_tp = state
+                }
+            }
+        }
+        if send_tp {
+            // println!("sending tp to channel");
+            uds_queue
+                .clone()
+                .send(CanioCmd::UdsCmdNoResponse(
+                    UDSProtocol::create_tp_msg(false).to_bytes(),
+                ))
+                .await?;
         }
 
         tokio::time::sleep(time::Duration::from_millis(10)).await;
-
     }
-
-    #[allow(unreachable_code)]
     Ok(())
 }
--- conUDS/src/modules/canio.rs
+++ conUDS/src/modules/canio.rs
@@ -1,30 +1,36 @@
-use std::marker::PhantomData;
-
 /// CANIO module
 use anyhow::{anyhow, Result};
 
+use ecu_diagnostics::channel::{ChannelResult, IsoTPChannel, IsoTPSettings};
 use ecu_diagnostics::hardware::{socketcan::SocketCanScanner, Hardware, HardwareScanner};
-use ecu_diagnostics::channel::{IsoTPChannel, IsoTPSettings};
-use ecu_diagnostics::uds::UDSProtocol;
-use ecu_diagnostics::dynamic_diag::DiagProtocol;
+use tokio::sync::mpsc::Receiver;
 
-pub struct CANIO<'a> {
-    channel: Box<dyn IsoTPChannel>,
-    _marker: PhantomData<&'a str>
+use crate::CanioCmd;
+
+struct IDs {
+    tx: u32,
+    #[allow(dead_code)]
+    rx: u32,
 }
 
-impl<'a> CANIO<'_> {
-    pub fn new(send_id: u32, recv_id: u32) -> Result<Self> {
+pub struct CANIO<'a> {
+    ids: IDs,
+    channel: Box<dyn IsoTPChannel>,
+    uds_queue: &'a mut Receiver<CanioCmd>,
+}
+
+impl<'a> CANIO<'a> {
+    pub fn new(send_id: u32, recv_id: u32, uds_queue: &'a mut Receiver<CanioCmd>) -> Result<Self> {
         let scanner = SocketCanScanner::new();
         let devices = scanner.list_devices();
 
-        if devices.len() == 0 {
+        if devices.is_empty() {
             return Err(anyhow!("No CAN devices detected"));
         }
-        println!("Devices: {:#?}", devices);
+        // println!("Devices: {:#?}", devices);
 
         let mut device = scanner.open_device_by_name("can0").unwrap();
-        println!("opened device: {:#?}", device);
+        // println!("opened device: {:#?}", device);
 
         let mut channel = device.create_iso_tp_channel().unwrap();
 
@@ -42,16 +48,54 @@ impl<'a> CANIO<'_> {
         channel.open()?;
 
         Ok(Self {
+            ids: IDs {
+                tx: send_id,
+                rx: recv_id,
+            },
             channel,
-            _marker: PhantomData,
+            uds_queue,
         })
     }
-}
 
-pub fn process() -> Result<()> {
-    let mut canio = CANIO::new(0x456, 0x123)?;
+    fn uds_send(&mut self, buf: &[u8], timeout_ms: u32) -> ChannelResult<()> {
+        self.channel.write_bytes(self.ids.tx, None, buf, timeout_ms)
+    }
 
-    canio.channel.write_bytes(0x456, None, &UDSProtocol::create_tp_msg(true).to_bytes(), 100)?;
+    #[allow(dead_code)]
+    fn uds_recv(&mut self, timeout_ms: u32) -> ChannelResult<Vec<u8>> {
+        self.channel.read_bytes(timeout_ms)
+    }
 
-    Ok(())
+    #[allow(dead_code)]
+    fn uds_send_recv(
+        &mut self,
+        buf: &[u8],
+        tx_timeout_ms: u32,
+        rx_timeout_ms: u32,
+    ) -> ChannelResult<Vec<u8>> {
+        self.channel
+            .read_write_bytes(self.ids.tx, None, buf, tx_timeout_ms, rx_timeout_ms)
+    }
+
+    pub async fn process(&mut self) -> Result<()> {
+        if let Ok(cmd) = self.uds_queue.try_recv() {
+            match cmd {
+                CanioCmd::UdsCmdNoResponse(msg) => {
+                    // println!("msg received from channel {:#?}", msg);
+                    let _ = self.uds_send(&msg, 5);
+                    // println!("result: {:#?}", res);
+                }
+                CanioCmd::UdsCmdWithResponse(msg, resp_channel) => {
+                    match self.uds_send_recv(&msg, 5, 5) {
+                        Ok(resp) => {
+                            let _ = resp_channel.send(resp);
+                        }
+                        Err(_) => {}
+                    }
+                }
+            }
+        }
+
+        Ok(())
+    }
 }
