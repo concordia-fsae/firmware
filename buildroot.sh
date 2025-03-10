concordia-fsae/firmware

commit f434bbbc79385ff0eadf2b55f110d8cb9a0a55bc

Added workflow target for github actions

buildroot.sh
@@ -77,5 +77,4 @@ IMAGE_TAG=${IMAGE_TAG} docker-compose run --rm buildroot $RUN_COMMAND
 EXIT_CODE=$?
 if [ $EXIT_CODE != 0 ]; then
     echo "Container exited with error code $EXIT_CODE..."
-    exit $EXIT_CODE;
 fi

.github/workflows/standard_checks.yml
@@ -27,3 +27,16 @@ jobs:
       build-targets: bl:1000,1001,1002,1003,1004,1005,1010,1011,1030,1031,1032
       flags: -j32 --package
       artifact-name: artifact-bootloader-updaters
+  validate-workflow: # TODO: Missing macos coverage, we would likely eed our own self
+                     # hosted macos runner todo this however
+    runs-on: self-hosted
+    needs: updaters
+    steps:
+      - name: Clean
+        uses: AutoModality/action-clean@1077775f5ef0022fc3a9d6f93377921ea3701fa7
+      - name: Pull Branch
+        id: pull
+        uses: concordia-fsae/firmware/.github/actions/pull/@master
+      - name: Enter Buildroot and Run Build
+        run: |
+          ./buildroot.sh -r "scons --targets=bmsb -j32"

commit ae9665e4a587d96e1c5d194196b8d7f7714eed76

Bugfix when running latest buildroot and wording fix
Issues introduced in f434bbbc

buildroot.sh
@@ -6,7 +6,7 @@ IMAGE_TAG=$CURRENT_TAG
 RUN_COMMAND=""
 
 print_help () {
-    echo "Usage 'buildroot.sh [OPTIONS] COMMAND"
+    echo "Usage 'buildroot.sh [OPTIONS]'"
     echo "Buildroot help..."
     echo "Options:"
     echo "  -h --help            Displays this help message"
@@ -16,7 +16,7 @@ print_help () {
 }
 
 validate_tag_change () {
-    if [ $IMAGE_TAG != $CURRENT_TAG ]; then \
+    if test "$IMAGE_TAG" != "$CURRENT_TAG" ; then \
         echo "Cannot specify tag and latest in the same command. Exiting..." && \
         exit 1;
     fi
@@ -38,12 +38,10 @@ do
             validate_tag_change
             shift
             IMAGE_TAG="$1"
-            shift
             ;;
         -r|--run)
             shift
             RUN_COMMAND="$1"
-            shift
             ;;
         -*|--*) # catch any unknown args and exit
             echo "Unknown option $1"
@@ -54,6 +52,7 @@ do
             break
             ;;
     esac
+    shift
 done
 
 for var in "$@"