7ef2037000999f3c98f4065da2ca6672807e315f
JoshLafleur <josh.lafleur@outlook.com>
2025-03-15 20:36:05 -0400

    Bringup TI TPS20XX HSD Driver and Timers
    Added support for timers
    Allow TPS20XX drivers to delay for a set amount of time before retrying
    a faulted HSD when auto-retry is enabled.

---
+++ components/shared/code/DRV/drv_timer.c
@@ -0,0 +1,81 @@
+/**
+ * @file drv_timer.c
+ * @brief  Source file for the timer library
+ */
+
+/******************************************************************************
+ *                             I N C L U D E S
+ ******************************************************************************/
+
+#include "drv_timer.h"
+#include "HW_tim.h"
+
+/******************************************************************************
+ *                       P U B L I C  F U N C T I O N S
+ ******************************************************************************/
+
+/**
+ * @brief Initialize a timer
+ * @param timer The timer to act upon
+ */
+void drv_timer_init(drv_timer_S * timer)
+{
+    timer->time_start_ms = 0U;
+    timer->runtime_ms = 0U;
+    timer->state = DRV_TIMER_STOPPED;
+}
+
+/**
+ * @brief Start a timer for the provided amount of time
+ * @param timer The timer to act upon
+ * @param runtime_ms The amount of time in ms the timer shall run before expiring.
+ */
+void drv_timer_start(drv_timer_S * timer, time_t runtime_ms)
+{
+    timer->time_start_ms = HW_TIM_getTimeMS();
+    timer->runtime_ms = runtime_ms;
+    timer->state = DRV_TIMER_RUNNING;
+}
+
+/**
+ * @brief Stop a timer
+ * @param timer The timer to act upon
+ */
+void drv_timer_stop(drv_timer_S * timer)
+{
+    timer->state = DRV_TIMER_STOPPED;
+}
+
+/**
+ * @brief Get the state of the timer
+ * @param timer The timer to retrieve the state of
+ * @return the active state of the timer
+ */
+drv_timer_state_E drv_timer_getState(drv_timer_S * timer)
+{
+    if ((timer->state == DRV_TIMER_RUNNING) &&
+        (timer->runtime_ms <= drv_timer_getElapsedTimeMs(timer)))
+    {
+        timer->state = DRV_TIMER_EXPIRED;
+    }
+
+    return timer->state;
+}
+
+/**
+ * @brief Get the elapsed time in ms of the timer
+ * @note If a timer is stopped, it will report an elapsed time of 0
+ * @param timer The timer to get the elapsed time of
+ * @return The time in ms since the timer was stopped.
+ */
+time_t drv_timer_getElapsedTimeMs(drv_timer_S *timer)
+{
+    time_t delta = 0U;
+
+    if (timer->state != DRV_TIMER_STOPPED)
+    {
+        delta = HW_TIM_getTimeMS() - timer->time_start_ms;
+    }
+
+    return delta;
+}
---
+++ components/shared/code/DRV/drv_tps20xx.h
@@ -0,0 +1,87 @@
+/**
+ * @file drv_tps20xx.h
+ * @brief  Header file for the TI TPS20xx driver
+ *
+ * @note To use this driver, a "drv_tpx20xx_componentSpecific.h" file should be
+ * created in a build include directory which must define the TPS20XX channels.
+ * A channel is a single output from any given IC. Due to the architecture of the
+ * HSD, there is no differentiation between the models where there are 1, 2, 3...
+ * channels on the same chip. If there are multiple chips, all used channels
+ * shall be defined in the channel configuration. This driver provides the interface
+ * for application code to enable, disable, and observe the state of any TPS20XX
+ * channel.
+ *
+ * Setup
+ * 1. Define the drv_tps20xx_channel_E type in drv_tpx20xx_componentSpecific.h
+ * 2. Declare the 'drv_tps20xx_channel_S drv_tps20xx_channels[DRV_TPS20XX_CHANNEL_COUNT]'
+ *    array in drv_tps20xx_componentSpecific.c in the project source. Configure
+ *    each channel to have the correct states
+ * 3. Call the run function periodically depending on the needs of the application.
+ *
+ * Usage
+ * - Channels are initialized in their off state
+ * - To en/disable a channel, the setEnabled function will need to be called on the
+ *   channel with the respective true or false for on and off respectively. The driver
+ *   will handle the pin state based upon the inverted logic of the channel and
+ *   the specific output pin configuration.
+ * - To evaluate the current state of a given channel, the getState function
+ *   shall be called
+ * - Once a fault is reported by the IC, the tps20xx channel will be disabled.
+ *   A faulted channel enters the over current (OC) state initially. If the
+ *   channel remains faulted, the channel tripped due to an overtemperature condition. 
+ *   An overtemperature condition can only be cleared once the chip has determined
+ *   it has cooled down enough.
+ * - When a channel is configured with auto_reset enabled, it will automatically
+ *   recover from a fault after the time set in retry_wait_ms. If a channel does
+ *   not have auto_reset enabled, the application shall disable the channel manually.
+ *   If the channel is in a fault state, the channel will reset to
+ *   the off state automatically if it has been disabled by the application and a
+ *   fault is no longer present.
+ */
+
+#pragma once
+
+/******************************************************************************
+ *                             I N C L U D E S
+ ******************************************************************************/
+
+#include "drv_tps20xx_componentSpecific.h"
+#include "HW_gpio.h"
+#include "stdbool.h"
+
+/******************************************************************************
+ *                             T Y P E D E F S
+ ******************************************************************************/
+
+typedef struct
+{
+    const HW_GPIO_pinmux_E enable;
+    const bool             inverted_enable_logic;
+    const HW_GPIO_pinmux_E fault;
+    bool                   auto_reset;
+    uint16_t               retry_wait_ms;
+} drv_tps20xx_channelConfig_S;
+
+extern drv_tps20xx_channelConfig_S drv_tps20xx_channels[DRV_TPS20XX_CHANNEL_COUNT];
+
+typedef enum
+{
+    DRV_TPS20XX_STATE_INIT = 0x00U,
+    DRV_TPS20XX_STATE_OFF,
+    DRV_TPS20XX_STATE_ENABLED,
+    DRV_TPS20XX_STATE_FAULTED_OC,
+    DRV_TPS20XX_STATE_FAULTED_OT,
+    DRV_TPS20XX_STATE_RETRY,
+    DRV_TPS20XX_STATE_ERROR,
+} drv_tps20xx_state_E;
+
+/******************************************************************************
+ *            P U B L I C  F U N C T I O N  P R O T O T Y P E S
+ ******************************************************************************/
+
+HW_StatusTypeDef_E  drv_tps20xx_init(void);
+HW_StatusTypeDef_E  drv_tps20xx_deInit(void);
+void                drv_tps20xx_run(void);
+
+drv_tps20xx_state_E drv_tps20xx_getState(drv_tps20xx_channel_E channel);
+void                drv_tps20xx_setEnabled(drv_tps20xx_channel_E channel, bool enabled);
