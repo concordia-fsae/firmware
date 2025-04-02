e08aaca4ac8bfe79c93c24d0279f040175d5ee23
JoshLafleur <josh.lafleur@outlook.com>
2025-04-01 23:42:52 -0400

    Update usage of input driver in tps_20xx

--- components/shared/code/DRV/drv_tps20xx.c
+++ components/shared/code/DRV/drv_tps20xx.c
@@ -257,5 +257,5 @@ static void drv_tps20xx_private_setICEnabled(drv_tps20xx_channel_E channel, bool
  */
 static bool drv_tps20xx_private_getICFaulted(drv_tps20xx_channel_E channel)
 {
-    return drv_inputAD_getLogicLevel(drv_tps20xx_channels[channel].fault) == DRV_INPUTAD_LOGIC_LOW; // TPS20xx has active low fault output
+    return drv_inputAD_getDigitalActiveState(drv_tps20xx_channels[channel].fault) == DRV_IO_ACTIVE;
 }
