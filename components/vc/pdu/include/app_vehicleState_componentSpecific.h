376bfb93a3cababefea49251b0083d80ab57e607
Josh Lafleur <josh.lafleur@flyzipline.com>
2025-04-13 12:13:46 -0700

    Bringup app_vehicleState Leader on VCPDU

---
+++ components/vc/pdu/include/app_vehicleState_componentSpecific.h
@@ -0,0 +1,14 @@
+/**
+ * @file app_vehicleState_componentSpecific.h
+ * @brief Header file for the component specific vehicle state application
+ */
+
+#pragma once
+
+/******************************************************************************
+ *                              D E F I N E S
+ ******************************************************************************/
+
+#define VEHICLESTATE_INPUTAD_TSMS DRV_INPUTAD_TSCHG_MS
+#define VEHICLESTATE_INPUTAD_RUN_BUTTON DRV_INPUTAD_RUN_BUTTON
+#define VEHICLESTATE_CANRX_CONTACTORSTATE CANRX_get_signal_func(VEH, BMSB_packContactorState)
