5c089f146c60b0c8dbf723631178b78dc6f6e8ff
lucas-graham <saculiomaharg@gmail.com>
2025-06-14 09:05:25 -0400

    Hotfix VN9008 Enable
    Sets the VN9008 enable function in cooling10Hz_PRD

--- components/vc/pdu/src/cooling.c
+++ components/vc/pdu/src/cooling.c
@@ -35,20 +35,21 @@ static void cooling10Hz_PRD(void)
     {
         // TODO: Make less ret
         drv_vn9008_setEnabled(DRV_VN9008_CHANNEL_FAN, true);
-#if FEATURE_IS_ENABLED(FEATURE_PUMP_FULL_BEANS)
+//#if FEATURE_IS_ENABLED(FEATURE_PUMP_FULL_BEANS)
         drv_outputAD_setDigitalActiveState(DRV_OUTPUTAD_PWM1, DRV_IO_ACTIVE);
-#else
+//#else
         HW_TIM_setDuty(HW_TIM_PORT_PUMP, HW_TIM_CHANNEL_1, 0.75f);
-#endif
+//#endif
     }
     else
     {
         drv_vn9008_setEnabled(DRV_VN9008_CHANNEL_FAN, false);
-#if FEATURE_IS_ENABLED(FEATURE_PUMP_FULL_BEANS)
+        drv_vn9008_setEnabled(DRV_VN9008_CHANNEL_PUMP, true);
+//#if FEATURE_IS_ENABLED(FEATURE_PUMP_FULL_BEANS)
+//#else
+        HW_TIM_setDuty(HW_TIM_PORT_PUMP, HW_TIM_CHANNEL_1, 0.5f);
         drv_outputAD_setDigitalActiveState(DRV_OUTPUTAD_PWM1, DRV_IO_ACTIVE);
-#else
-        HW_TIM_setDuty(HW_TIM_PORT_PUMP, HW_TIM_CHANNEL_1, 0.00f);
-#endif
+//#endif
     }
 }
 
