0d3af73f3f142a05f089507bf29ea894a1d58cfa
Louiscars190 <louiscars170@gmail.com>
2025-06-30 18:33:58 -0400

    Ready to merge, has been tested and confirmed to work, all bugs worked out for now This is a combination of 3 commits.
    Squash of 5 commits, ready for pull request# This is a combination of 5 commits.
    
    Initial commit for Steering Angle Sensor
    	- Has network stuff
    	- Has code + calls adc
    
    forgot to add the voltage divider compensation
    
    Changes to CAN Network for Steering Angle Sensor
    	- Moved signal definition from shared template to vcfront's vcfront signals
    	- Changed from 12 bit to 9 bit, reflecting resolution decrease from 0.1 to 1.
    
    Changes to Steering Angle ADC
    	- Changed name from SPARE2 to STR_ANGLE
    	- Remomved unnecessary spaces and lines
    
    Changes to steeringAngle.c
    	- Changed comments from c comments to Doxygen template
    
    Removed useless spaces from vcfront-message.yaml
    
    Re-add signals.yaml
    
    Switched signals for diff
    
    Changed max to 172 because resolution is 1
    
    extra line
    
    Changed manual transfer function+edge-cases for interpolation library
    
    Removed .results and added lib_interpolation_init for initialization
    Removed resolution from vcfront-signals.yaml because auto calculates based on bitwidth
    
    added voltage output and calibrated the sensor
    
    Re-added Changes to resolution in CANbus VCFront and removed STR_ANGLE to SPARE2 of rear analog input
    
    tunned steering angle sensor

--- components/vc/front/include/CANIO_componentSpecific.h
+++ components/vc/front/include/CANIO_componentSpecific.h
@@ -26,6 +26,7 @@
 #include "brakePressure.h"
 #include "drv_tps20xx.h"
 #include "cockpitLights.h"
+#include "steeringAngle.h"
 
 /******************************************************************************
  *                              D E F I N E S
@@ -75,5 +76,7 @@
 #define set_5vExtHsdState(m,b,n,s) set(m,b,n,s, drv_tps20xx_getStateCAN(DRV_TPS20XX_CHANNEL_5V_EXT))
 #define set_bmsLightState(m,b,n,s) set(m,b,n,s, cockpitLights_bms_getStateCAN())
 #define set_imdLightState(m,b,n,s) set(m,b,n,s, cockpitLights_imd_getStateCAN())
+#define set_steeringAngle(m,b,n,s) set (m,b,n,s, steeringAngle_getSteeringAngle())
+#define set_steeringAngleVoltage(m,b,n,s) set (m,b,n,s,steeringAngle_getSteeringVoltage())
 
 #include "TemporaryStubbing.h"
