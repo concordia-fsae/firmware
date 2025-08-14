concordia-fsae/firmware

commit 12948905aaa43a2ada48415f12da92a1c5ebd90a

vcpdu: Bringing shutdown circuit message

components/vc/pdu/include/CANIO_componentSpecific.h
@@ -62,5 +62,24 @@
 #define set_bms6HsdState(m,b,n,s) set(m,b,n,s, drv_hsd_getCANState(drv_tps2hb16ab_getState(DRV_TPS2HB16AB_IC_BMS5_BMS6, DRV_TPS2HB16AB_OUT_2)))
 #define set_glvVoltage(m,b,n,s) set(m,b,n,s, powerManager_getGLVVoltage())
 #define set_glvCurrent(m,b,n,s) set(m,b,n,s, powerManager_getGLVCurrent())
+#define set_runButton(m,b,n,s) set(m,b,n,s, (drv_inputAD_getDigitalActiveState(DRV_INPUTAD_RUN_BUTTON) == DRV_IO_ACTIVE) ? \
+                                             CAN_DIGITALSTATUS_ON: CAN_DIGITALSTATUS_OFF)
+#define set_pduSafetyReset(m,b,n,s) set(m,b,n,s, (drv_inputAD_getDigitalActiveState(DRV_INPUTAD_VCU_SFTY_RESET) == DRV_IO_ACTIVE) ? \
+                                                 CAN_DIGITALSTATUS_ON: CAN_DIGITALSTATUS_OFF)
+#define set_bmsSafetyReset(m,b,n,s) set(m,b,n,s, (drv_inputAD_getDigitalActiveState(DRV_INPUTAD_BMS_RESET) == DRV_IO_ACTIVE) ? \
+                                                 CAN_DIGITALSTATUS_ON: CAN_DIGITALSTATUS_OFF)
+#define set_safetyReset(m,b,n,s) set(m,b,n,s, ((drv_inputAD_getDigitalActiveState(DRV_INPUTAD_VCU_SFTY_RESET) == DRV_IO_ACTIVE) && \
+                                               (drv_inputAD_getDigitalActiveState(DRV_INPUTAD_BMS_RESET) == DRV_IO_ACTIVE)) ? \
+                                               CAN_DIGITALSTATUS_ON: CAN_DIGITALSTATUS_OFF)
+#define set_bmsbSafetyStatus(m,b,n,s) set(m,b,n,s, (drv_inputAD_getDigitalActiveState(DRV_INPUTAD_IMD_SAFETY_EN) == DRV_IO_ACTIVE) ? \
+                                                    CAN_SHUTDOWNCIRCUITSTATUS_CLOSED: CAN_SHUTDOWNCIRCUITSTATUS_OPEN)
+#define set_imdSafetyStatus(m,b,n,s) set(m,b,n,s, (drv_inputAD_getDigitalActiveState(DRV_INPUTAD_BMS_SAFETY_EN) == DRV_IO_ACTIVE) ? \
+                                                   CAN_SHUTDOWNCIRCUITSTATUS_CLOSED: CAN_SHUTDOWNCIRCUITSTATUS_OPEN)
+#define set_bspdSafetyStatus(m,b,n,s) set(m,b,n,s, (drv_inputAD_getDigitalActiveState(DRV_INPUTAD_BSPD_MEM) == DRV_IO_ACTIVE) ? \
+                                                    CAN_SHUTDOWNCIRCUITSTATUS_CLOSED: CAN_SHUTDOWNCIRCUITSTATUS_OPEN)
+#define set_vcSafetyStatus(m,b,n,s) set(m,b,n,s, (drv_outputAD_getDigitalActiveState(DRV_OUTPUTAD_VCU_SFTY_EN) == DRV_IO_ACTIVE) ? \
+                                                  CAN_SHUTDOWNCIRCUITSTATUS_CLOSED: CAN_SHUTDOWNCIRCUITSTATUS_OPEN)
+#define set_tsmsSafetyStatus(m,b,n,s) set(m,b,n,s, (drv_inputAD_getDigitalActiveState(DRV_INPUTAD_TSCHG_MS) == DRV_IO_ACTIVE) ? \
+                                                    CAN_DIGITALSTATUS_ON: CAN_DIGITALSTATUS_OFF)
 
 #include "TemporaryStubbing.h"

components/vc/pdu/include/HW/drv_inputAD_componentSpecific.h
@@ -13,6 +13,9 @@ typedef enum
 {
     DRV_INPUTAD_TSCHG_MS = 0U,
     DRV_INPUTAD_RUN_BUTTON,
+    DRV_INPUTAD_IMD_SAFETY_EN,
+    DRV_INPUTAD_BMS_SAFETY_EN,
+    DRV_INPUTAD_BMS_RESET,
     DRV_INPUTAD_5V_NFLT1,
     DRV_INPUTAD_5V_NFLT2,
     DRV_INPUTAD_VCU_SFTY_RESET,

components/vc/pdu/src/HW/drv_inputAD_componentSpecific.c
@@ -35,7 +35,19 @@ drv_inputAD_configDigital_S drv_inputAD_configDigital[DRV_INPUTAD_DIGITAL_COUNT]
     },
     [DRV_INPUTAD_RUN_BUTTON] = {
         .type = INPUT_DIGITAL_CAN,
-        .config.canrx_digitalStatus = &CANRX_get_signal_func(VEH, VCFRONT_runButtonStatus),
+        .config.canrx_digitalStatus = &CANRX_get_signal_func(VEH, STW_runButton),
+    },
+    [DRV_INPUTAD_IMD_SAFETY_EN] = {
+        .type = INPUT_DIGITAL_CAN,
+        .config.canrx_digitalStatus = &CANRX_get_signal_func(VEH, BMSB_imdStatusMem),
+    },
+    [DRV_INPUTAD_BMS_SAFETY_EN] = {
+        .type = INPUT_DIGITAL_CAN,
+        .config.canrx_digitalStatus = &CANRX_get_signal_func(VEH, BMSB_bmsStatusMem),
+    },
+    [DRV_INPUTAD_BMS_RESET] = {
+        .type = INPUT_DIGITAL_CAN,
+        .config.canrx_digitalStatus = &CANRX_get_signal_func(VEH, BMSB_bmsIMDReset),
     },
     [DRV_INPUTAD_5V_NFLT1] = { 
         .type = INPUT_DIGITAL,
@@ -62,7 +74,7 @@ drv_inputAD_configDigital_S drv_inputAD_configDigital[DRV_INPUTAD_DIGITAL_COUNT]
         .type = INPUT_DIGITAL,
         .config.gpio = {
             .pin = HW_GPIO_BSPD_MEM,
-            .active_level = DRV_IO_LOGIC_LOW,
+            .active_level = DRV_IO_LOGIC_HIGH,
         },
     },
 };

network/definition/data/components/vcpdu/vcpdu-message.yaml
@@ -20,6 +20,24 @@ messages:
       vehicleState:
       glvVoltage:
       glvCurrent:
+      runButton:
+        template: digitalStatus
+      pduSafetyReset:
+        template: digitalStatus
+      bmsSafetyReset:
+        template: digitalStatus
+      safetyReset:
+        template: digitalStatus
+      bmsbSafetyStatus:
+        template: shutdownRelayStatus
+      imdSafetyStatus:
+        template: shutdownRelayStatus
+      bspdSafetyStatus:
+        template: shutdownRelayStatus
+      vcSafetyStatus:
+        template: shutdownRelayStatus
+      tsmsSafetyStatus:
+        template: digitalStatus
 
   hsdState:
     description: Current state of the HSDs

network/definition/data/components/vcpdu/vcpdu-rx.yaml
@@ -5,9 +5,15 @@ messages:
 signals:
   BMSB_packContactorState:
     sourceBuses: veh
+  BMSB_imdStatusMem:
+    sourceBuses: veh
+  BMSB_bmsStatusMem:
+    sourceBuses: veh
+  BMSB_bmsIMDReset:
+    sourceBuses: veh
   BMSB_tsmsChg:
     sourceBuses: veh
-  VCFRONT_runButtonStatus:
+  STW_runButton:
     sourceBuses: veh
   VCFRONT_brakePosition:
     sourceBuses: veh

network/definition/data/templates/signals.yaml
@@ -43,3 +43,9 @@ signals:
         min: 0
         max: 2000
     continuous: true
+  shutdownRelayStatus:
+    description: State of Safety Relay
+    discreteValues: shutdownCircuitStatus
+  digitalStatus:
+    description: Digital Status
+    discreteValues: digitalStatus

network/definition/discrete_values.yaml
@@ -160,3 +160,8 @@ pm100dxEepromAddress:
 pm100dxEepromRWCommand:
   "READ": 0
   "WRITE": 1
+
+shutdownCircuitStatus:
+  "SNA": 0
+  "OPEN": 1
+  "CLOSED": 2

commit a8b114c6b623ae391422a306cc990ac92a8a8a6e

vcpdu: Fix BMS/IMD shutdown status source
fixes: 12948905aaa43a2ada48415f12da92a1c5ebd90a

components/vc/pdu/include/CANIO_componentSpecific.h
@@ -71,9 +71,9 @@
 #define set_safetyReset(m,b,n,s) set(m,b,n,s, ((drv_inputAD_getDigitalActiveState(DRV_INPUTAD_VCU_SFTY_RESET) == DRV_IO_ACTIVE) && \
                                                (drv_inputAD_getDigitalActiveState(DRV_INPUTAD_BMS_RESET) == DRV_IO_ACTIVE)) ? \
                                                CAN_DIGITALSTATUS_ON: CAN_DIGITALSTATUS_OFF)
-#define set_bmsbSafetyStatus(m,b,n,s) set(m,b,n,s, (drv_inputAD_getDigitalActiveState(DRV_INPUTAD_IMD_SAFETY_EN) == DRV_IO_ACTIVE) ? \
+#define set_bmsbSafetyStatus(m,b,n,s) set(m,b,n,s, (drv_inputAD_getDigitalActiveState(DRV_INPUTAD_BMS_SAFETY_EN) == DRV_IO_ACTIVE) ? \
                                                     CAN_SHUTDOWNCIRCUITSTATUS_CLOSED: CAN_SHUTDOWNCIRCUITSTATUS_OPEN)
-#define set_imdSafetyStatus(m,b,n,s) set(m,b,n,s, (drv_inputAD_getDigitalActiveState(DRV_INPUTAD_BMS_SAFETY_EN) == DRV_IO_ACTIVE) ? \
+#define set_imdSafetyStatus(m,b,n,s) set(m,b,n,s, (drv_inputAD_getDigitalActiveState(DRV_INPUTAD_IMD_SAFETY_EN) == DRV_IO_ACTIVE) ? \
                                                    CAN_SHUTDOWNCIRCUITSTATUS_CLOSED: CAN_SHUTDOWNCIRCUITSTATUS_OPEN)
 #define set_bspdSafetyStatus(m,b,n,s) set(m,b,n,s, (drv_inputAD_getDigitalActiveState(DRV_INPUTAD_BSPD_MEM) == DRV_IO_ACTIVE) ? \
                                                     CAN_SHUTDOWNCIRCUITSTATUS_CLOSED: CAN_SHUTDOWNCIRCUITSTATUS_OPEN)