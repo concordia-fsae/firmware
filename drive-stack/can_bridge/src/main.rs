537c99798a9efcbaf64ee8a9be3f2aa9bb4972c6
JoshLafleur <josh.lafleur@outlook.com>
2025-09-03 12:42:27 -0400

    can-bridge: Basic support for multi-threaded can bridge

--- drive-stack/can_bridge/src/main.rs
+++
@@ -1,9 +0,0 @@
-use socketcan;
-
-fn main() {
-    println!("CAN bridge Rust test");
-    println!("target = {}-{}-{}", std::env::consts::ARCH, std::env::consts::OS, std::env::consts::FAMILY);
-
-    #[cfg(target_arch = "x86_64")]
-    println!("OK: compiled for x86_64");
-}
