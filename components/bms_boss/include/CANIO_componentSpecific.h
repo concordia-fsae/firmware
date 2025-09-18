dbef6dd024a26f697fe277e77643b012f53ecc69
Nurtrantpoem19 <nurtrantpoem19@gmail.com>
2025-09-18 12:10:49 -0400

    add BMSB_mcu temp to bms_boss yaml and header files

--- components/bms_boss/include/CANIO_componentSpecific.h
+++ components/bms_boss/include/CANIO_componentSpecific.h
@@ -1,5 +1,5 @@
 /**
- RX_config* CAN.h
+ * RX_config* CAN.h
  * Header file for CANRX configuration
  */
 
@@ -18,70 +18,71 @@
 
 // imports for data access
 #include "BMS.h"
-#include "Sys.h"
-#include "IMD.h"
-#include "ENV.h"
-#include "Module.h"
 #include "drv_inputAD.h"
 #include "drv_outputAD.h"
+#include "ENV.h"
+#include "IMD.h"
+#include "Module.h"
+#include "Sys.h"
 
 /******************************************************************************
  *          P R I V A T E  F U N C T I O N  P R O T O T Y P E S
  ******************************************************************************/
 
-uint8_t CANIO_tx_getNLG513ControlByte(void);
-uint8_t CANIO_tx_getElconControlByte(void);
+uint8_t                       CANIO_tx_getNLG513ControlByte(void);
+uint8_t                       CANIO_tx_getElconControlByte(void);
 CAN_prechargeContactorState_E CANIO_tx_getContactorState(void);
 
 /******************************************************************************
  *                              D E F I N E S
  ******************************************************************************/
 
-#define CANIO_UDS_BUFFER_LENGTH 8U
-#define CANIO_getTimeMs() (HW_TIM_getTimeMS())
+#define CANIO_UDS_BUFFER_LENGTH                   8U
+#define CANIO_getTimeMs()                         (HW_TIM_getTimeMS())
 
-#define set_criticalDataCounter(m,b,n,s) set(m,b,n,s, cantx_counter.counter_100Hz)
-#define set_packChargeLimit(m,b,n,s) set(m,b,n,s, BMS.pack_charge_limit)
-#define set_packDischargeLimit(m,b,n,s) set(m,b,n,s, BMS.pack_discharge_limit)
-#define set_packVoltage(m,b,n,s) set(m,b,n,s, BMS.pack_voltage)
-#define set_packCurrent(m,b,n,s) set(m,b,n,s, BMS.pack_current)
-#define set_packContactorState(m,b,n,s) set(m,b,n,s, CANIO_tx_getContactorState())
-#define set_nlg513ControlByte(m,b,n,s) set(m,b,n,s, CANIO_tx_getNLG513ControlByte())
-#define set_nlg513MaxMainsCurrent(m,b,n,s) set(m,b,n,s, 16.0f)
-#define set_nlg513MaxChargeVoltage(m,b,n,s) set(m,b,n,s, BMS_CONFIGURED_PACK_MAX_VOLTAGE)
-#define set_nlg513MaxChargeCurrent(m,b,n,s) set(m,b,n,s, (CANIO_tx_getNLG513ControlByte() == 0x40) ? 0.0f : BMS.pack_charge_limit)
-#define transmit_BMSB_brusaChargeCommand (SYS_SFT_checkBrusaChargerTimeout() == false)
-#define set_taskUsage1kHz(m,b,n,s) set(m,b,n,s, Module_getTotalRuntimePercentage(MODULE_1kHz_TASK));
-#define set_taskUsage100Hz(m,b,n,s) set(m,b,n,s, Module_getTotalRuntimePercentage(MODULE_100Hz_TASK));
-#define set_taskUsage10Hz(m,b,n,s) set(m,b,n,s, Module_getTotalRuntimePercentage(MODULE_10Hz_TASK));
-#define set_taskUsage1Hz(m,b,n,s) set(m,b,n,s, Module_getTotalRuntimePercentage(MODULE_1Hz_TASK));
-#define set_taskUsageIdle(m,b,n,s) set(m,b,n,s, Module_getTotalRuntimePercentage(MODULE_IDLE_TASK));
-#define set_elconMaxChargeVoltage(m,b,n,s) set(m,b,n,s, BMS_CONFIGURED_PACK_MAX_VOLTAGE)
-#define set_elconMaxChargeCurrent(m,b,n,s) set(m,b,n,s, BMS.pack_charge_limit)
-#define set_elconControlByte(m,b,n,s) set(m,b,n,s, CANIO_tx_getElconControlByte())
-#define transmit_BMSB_elconChargeCommand (SYS_SFT_checkElconChargerTimeout() == false)
-#define transmit_BMSB_currentLimit (SYS_SFT_checkMCTimeout() == false)
-#define set_maxCharge(m,b,n,s) set(m,b,n,s, BMS.pack_charge_limit);
-#define set_maxDischarge(m,b,n,s) set(m,b,n,s, BMS.pack_discharge_limit);
-#define set_packRH(m,b,n,s) set(m,b,n,s, ENV.board.rh)
-#define set_packTemperature(m,b,n,s) set(m,b,n,s, ENV.board.ambient_temp)
-#define set_taskIterations1kHz(m,b,n,s)          set(m,b,n,s, (uint16_t)Module_getTotalRuntimeIterations(MODULE_1kHz_TASK))
-#define set_taskIterations100Hz(m,b,n,s)         set(m,b,n,s, (uint16_t)Module_getTotalRuntimeIterations(MODULE_100Hz_TASK))
-#define set_taskIterations10Hz(m,b,n,s)          set(m,b,n,s, (uint16_t)Module_getTotalRuntimeIterations(MODULE_10Hz_TASK))
-#define set_taskIterations1Hz(m,b,n,s)           set(m,b,n,s, (uint16_t)Module_getTotalRuntimeIterations(MODULE_1Hz_TASK))
-#define set_tsmsChg(m,b,n,s) set(m,b,n,s, (drv_inputAD_getDigitalActiveState(DRV_INPUTAD_DIGITAL_TSMS_CHG) == DRV_IO_ACTIVE) ? \
-                                           CAN_DIGITALSTATUS_ON : CAN_DIGITALSTATUS_OFF)
-#define set_okHS(m,b,n,s) set(m,b,n,s, (drv_inputAD_getDigitalActiveState(DRV_INPUTAD_DIGITAL_OK_HS) == DRV_IO_ACTIVE) ? \
-                                        CAN_DIGITALSTATUS_ON : CAN_DIGITALSTATUS_OFF)
-#define set_bmsIMDReset(m,b,n,s) set(m,b,n,s, (drv_inputAD_getDigitalActiveState(DRV_INPUTAD_DIGITAL_BMS_IMD_RESET) == DRV_IO_ACTIVE) ? \
-                                               CAN_DIGITALSTATUS_ON : CAN_DIGITALSTATUS_OFF)
-#define set_imdStatusMem(m,b,n,s) set(m,b,n,s, (drv_inputAD_getDigitalActiveState(DRV_INPUTAD_DIGITAL_IMD_STATUS_MEM) == DRV_IO_ACTIVE) ? \
-                                                CAN_DIGITALSTATUS_ON : CAN_DIGITALSTATUS_OFF)
-#define set_bmsStatusMem(m,b,n,s) set(m,b,n,s, (drv_inputAD_getDigitalActiveState(DRV_INPUTAD_DIGITAL_BMS_STATUS_MEM) == DRV_IO_ACTIVE) ? \
-                                                CAN_DIGITALSTATUS_ON : CAN_DIGITALSTATUS_OFF)
-#define set_bmsStatus(m,b,n,s) set(m,b,n,s, (drv_outputAD_getDigitalActiveState(DRV_OUTPUTAD_DIGITAL_STATUS_BMS) == DRV_IO_ACTIVE) ?\
-                                             CAN_DIGITALSTATUS_ON : CAN_DIGITALSTATUS_OFF)
-#define set_imdStatus(m,b,n,s) set(m,b,n,s, (drv_outputAD_getDigitalActiveState(DRV_OUTPUTAD_DIGITAL_STATUS_IMD) == DRV_IO_ACTIVE) ?\
-                                             CAN_DIGITALSTATUS_ON : CAN_DIGITALSTATUS_OFF)
-#define set_packCSVoltage(m,b,n,s) set(m,b,n,s, drv_inputAD_getAnalogVoltage(DRV_INPUTAD_ANALOG_CS))
+#define set_criticalDataCounter(m, b, n, s)       set(m, b, n, s, cantx_counter.counter_100Hz)
+#define set_packChargeLimit(m, b, n, s)           set(m, b, n, s, BMS.pack_charge_limit)
+#define set_packDischargeLimit(m, b, n, s)        set(m, b, n, s, BMS.pack_discharge_limit)
+#define set_packVoltage(m, b, n, s)               set(m, b, n, s, BMS.pack_voltage)
+#define set_packCurrent(m, b, n, s)               set(m, b, n, s, BMS.pack_current)
+#define set_packContactorState(m, b, n, s)        set(m, b, n, s, CANIO_tx_getContactorState())
+#define set_nlg513ControlByte(m, b, n, s)         set(m, b, n, s, CANIO_tx_getNLG513ControlByte())
+#define set_nlg513MaxMainsCurrent(m, b, n, s)     set(m, b, n, s, 16.0f)
+#define set_nlg513MaxChargeVoltage(m, b, n, s)    set(m, b, n, s, BMS_CONFIGURED_PACK_MAX_VOLTAGE)
+#define set_nlg513MaxChargeCurrent(m, b, n, s)    set(m, b, n, s, (CANIO_tx_getNLG513ControlByte() == 0x40) ? 0.0f : BMS.pack_charge_limit)
+#define transmit_BMSB_brusaChargeCommand          (SYS_SFT_checkBrusaChargerTimeout() == false)
+#define set_taskUsage1kHz(m, b, n, s)             set(m, b, n, s, Module_getTotalRuntimePercentage(MODULE_1kHz_TASK));
+#define set_taskUsage100Hz(m, b, n, s)            set(m, b, n, s, Module_getTotalRuntimePercentage(MODULE_100Hz_TASK));
+#define set_taskUsage10Hz(m, b, n, s)             set(m, b, n, s, Module_getTotalRuntimePercentage(MODULE_10Hz_TASK));
+#define set_taskUsage1Hz(m, b, n, s)              set(m, b, n, s, Module_getTotalRuntimePercentage(MODULE_1Hz_TASK));
+#define set_taskUsageIdle(m, b, n, s)             set(m, b, n, s, Module_getTotalRuntimePercentage(MODULE_IDLE_TASK));
+#define set_elconMaxChargeVoltage(m, b, n, s)     set(m, b, n, s, BMS_CONFIGURED_PACK_MAX_VOLTAGE)
+#define set_elconMaxChargeCurrent(m, b, n, s)     set(m, b, n, s, BMS.pack_charge_limit)
+#define set_elconControlByte(m, b, n, s)          set(m, b, n, s, CANIO_tx_getElconControlByte())
+#define transmit_BMSB_elconChargeCommand          (SYS_SFT_checkElconChargerTimeout() == false)
+#define transmit_BMSB_currentLimit                (SYS_SFT_checkMCTimeout() == false)
+#define set_maxCharge(m, b, n, s)                 set(m, b, n, s, BMS.pack_charge_limit);
+#define set_maxDischarge(m, b, n, s)              set(m, b, n, s, BMS.pack_discharge_limit);
+#define set_packRH(m, b, n, s)                    set(m, b, n, s, ENV.board.rh)
+#define set_packTemperature(m, b, n, s)           set(m, b, n, s, ENV.board.ambient_temp)
+#define set_mcuTemperature(m, b, n, s)            set(m, b, n, s, ENV.board.mcu_temp)
+#define set_taskIterations1kHz(m, b, n, s)        set(m, b, n, s, (uint16_t)Module_getTotalRuntimeIterations(MODULE_1kHz_TASK))
+#define set_taskIterations100Hz(m, b, n, s)       set(m, b, n, s, (uint16_t)Module_getTotalRuntimeIterations(MODULE_100Hz_TASK))
+#define set_taskIterations10Hz(m, b, n, s)        set(m, b, n, s, (uint16_t)Module_getTotalRuntimeIterations(MODULE_10Hz_TASK))
+#define set_taskIterations1Hz(m, b, n, s)         set(m, b, n, s, (uint16_t)Module_getTotalRuntimeIterations(MODULE_1Hz_TASK))
+#define set_tsmsChg(m, b, n, s)                   set(m, b, n, s, (drv_inputAD_getDigitalActiveState(DRV_INPUTAD_DIGITAL_TSMS_CHG) == DRV_IO_ACTIVE) ? \
+                                                      CAN_DIGITALSTATUS_ON : CAN_DIGITALSTATUS_OFF)
+#define set_okHS(m, b, n, s)                      set(m, b, n, s, (drv_inputAD_getDigitalActiveState(DRV_INPUTAD_DIGITAL_OK_HS) == DRV_IO_ACTIVE) ? \
+                                                      CAN_DIGITALSTATUS_ON : CAN_DIGITALSTATUS_OFF)
+#define set_bmsIMDReset(m, b, n, s)               set(m, b, n, s, (drv_inputAD_getDigitalActiveState(DRV_INPUTAD_DIGITAL_BMS_IMD_RESET) == DRV_IO_ACTIVE) ? \
+                                                      CAN_DIGITALSTATUS_ON : CAN_DIGITALSTATUS_OFF)
+#define set_imdStatusMem(m, b, n, s)              set(m, b, n, s, (drv_inputAD_getDigitalActiveState(DRV_INPUTAD_DIGITAL_IMD_STATUS_MEM) == DRV_IO_ACTIVE) ? \
+                                                      CAN_DIGITALSTATUS_ON : CAN_DIGITALSTATUS_OFF)
+#define set_bmsStatusMem(m, b, n, s)              set(m, b, n, s, (drv_inputAD_getDigitalActiveState(DRV_INPUTAD_DIGITAL_BMS_STATUS_MEM) == DRV_IO_ACTIVE) ? \
+                                                      CAN_DIGITALSTATUS_ON : CAN_DIGITALSTATUS_OFF)
+#define set_bmsStatus(m, b, n, s)                 set(m, b, n, s, (drv_outputAD_getDigitalActiveState(DRV_OUTPUTAD_DIGITAL_STATUS_BMS) == DRV_IO_ACTIVE) ? \
+                                                      CAN_DIGITALSTATUS_ON : CAN_DIGITALSTATUS_OFF)
+#define set_imdStatus(m, b, n, s)                 set(m, b, n, s, (drv_outputAD_getDigitalActiveState(DRV_OUTPUTAD_DIGITAL_STATUS_IMD) == DRV_IO_ACTIVE) ? \
+                                                      CAN_DIGITALSTATUS_ON : CAN_DIGITALSTATUS_OFF)
+#define set_packCSVoltage(m, b, n, s)             set(m, b, n, s, drv_inputAD_getAnalogVoltage(DRV_INPUTAD_ANALOG_CS))
 #include "TemporaryStubbing.h"
