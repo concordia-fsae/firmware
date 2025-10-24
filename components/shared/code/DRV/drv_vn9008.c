e5daa67d0d8e6d1494986b1e13a2e5371d99f793
Nurtrantpoem19 <nurtrantpoem19@gmail.com>
2025-10-23 20:51:38 -0400

    add check for previous run() states

--- components/shared/code/DRV/drv_vn9008.c
+++ components/shared/code/DRV/drv_vn9008.c
@@ -54,7 +54,6 @@ void drv_vn9008_run(void)
         const float32_t current        = cs_voltage * drv_vn9008_channels[i].cs_amp_per_volt;
         const bool      diagnostic     = drv_outputAD_getDigitalActiveState(drv_vn9008_channels[i].enable_cs) == DRV_IO_ACTIVE;
         drv_vn9008_data.current[i] = diagnostic ? current : drv_vn9008_data.current[i];
-        //
         const bool      is_overcurrent = drv_vn9008_data.current[i] > drv_vn9008_channels[i].current_limit_amp;
 
         if (is_overcurrent)
@@ -66,7 +65,14 @@ void drv_vn9008_run(void)
             }
             else if (state == DRV_TIMER_EXPIRED)
             {
-                drv_vn9008_data.state[i] = DRV_HSD_STATE_OVERCURRENT;
+                if (drv_vn9008_data.state[i] == DRV_HSD_STATE_OVERCURRENT)
+                {
+                    drv_vn9008_data.state[i] = DRV_HSD_STATE_OVERTEMP;
+                }
+                else
+                {
+                    drv_vn9008_data.state[i] = DRV_HSD_STATE_OVERCURRENT;
+                }
             }
         }
         else
