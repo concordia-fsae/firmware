2c57e7b60357db2467154a50e9808b14b146485d
JoshLafleur <josh.lafleur@outlook.com>
2025-11-02 19:19:31 -0500

    sws: Bringup userInput library

---
+++ components/sws/include/HW/drv_userInput_componentSpecific.h
@@ -0,0 +1,22 @@
+/**
+ * @file drv_userInput_componentSpecific.h
+ * @brief Abstraction layer for various user input devices
+ */
+
+#pragma once
+
+
+/******************************************************************************
+*                             T Y P E D E F S
+ ******************************************************************************/
+
+typedef enum
+{
+    USERINPUT_BUTTON_LEFT_TOP = 0x00U,
+    USERINPUT_BUTTON_LEFT_MID,
+    USERINPUT_BUTTON_LEFT_BOT,
+    USERINPUT_BUTTON_RIGHT_TOP,
+    USERINPUT_BUTTON_RIGHT_MID,
+    USERINPUT_BUTTON_RIGHT_BOT,
+    USERINPUT_BUTTON_COUNT,
+} drv_userInput_button_E;
---
+++ components/sws/src/HW/drv_userInput_componentSpecific.c
@@ -0,0 +1,77 @@
+/**
+ * @file drv_userInput.h
+ * @brief Abstraction layer for various user input devices
+ */
+
+/******************************************************************************
+ *                             I N C L U D E S
+ ******************************************************************************/
+
+#include "drv_userInput.h"
+
+/******************************************************************************
+ *                           P U B L I C  V A R S
+ ******************************************************************************/
+
+drv_userInput_configButton_S userInput_configButtons[USERINPUT_BUTTON_COUNT] = {
+    [USERINPUT_BUTTON_LEFT_TOP] = {
+        .type = USERINPUT_TYPE_GPIO,
+        .config = {
+            .gpio = {
+                .pin = DRV_INPUTAD_DIGITAL_CHANNEL_DIN1,
+                .debounce_on_ms = 250,
+                .debounce_off_ms = 100,
+            },
+        },
+    },
+    [USERINPUT_BUTTON_LEFT_MID] = {
+        .type = USERINPUT_TYPE_GPIO,
+        .config = {
+            .gpio = {
+                .pin = DRV_INPUTAD_DIGITAL_CHANNEL_DIN2,
+                .debounce_on_ms = 250,
+                .debounce_off_ms = 100,
+            },
+        },
+    },
+    [USERINPUT_BUTTON_LEFT_BOT] = {
+        .type = USERINPUT_TYPE_GPIO,
+        .config = {
+            .gpio = {
+                .pin = DRV_INPUTAD_DIGITAL_CHANNEL_DIN3,
+                .debounce_on_ms = 250,
+                .debounce_off_ms = 100,
+            },
+        },
+    },
+    [USERINPUT_BUTTON_RIGHT_TOP] = {
+        .type = USERINPUT_TYPE_GPIO,
+        .config = {
+            .gpio = {
+                .pin = DRV_INPUTAD_DIGITAL_CHANNEL_DIN4,
+                .debounce_on_ms = 250,
+                .debounce_off_ms = 100,
+            },
+        },
+    },
+    [USERINPUT_BUTTON_RIGHT_MID] = {
+        .type = USERINPUT_TYPE_GPIO,
+        .config = {
+            .gpio = {
+                .pin = DRV_INPUTAD_DIGITAL_CHANNEL_DIN5,
+                .debounce_on_ms = 250,
+                .debounce_off_ms = 100,
+            },
+        },
+    },
+    [USERINPUT_BUTTON_RIGHT_BOT] = {
+        .type = USERINPUT_TYPE_GPIO,
+        .config = {
+            .gpio = {
+                .pin = DRV_INPUTAD_DIGITAL_CHANNEL_DIN6,
+                .debounce_on_ms = 250,
+                .debounce_off_ms = 100,
+            },
+        },
+    },
+};
