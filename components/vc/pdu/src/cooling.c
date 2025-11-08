60ad0613f0a8c9e5541f806a07990328b44851c0
Nurtrantpoem19 <nurtrantpoem19@gmail.com>
2025-11-07 23:46:22 -0500

    redo vehicle state behaviour logic for fan
    ran uncrustify again

--- components/vc/pdu/src/cooling.c
+++ components/vc/pdu/src/cooling.c
@@ -54,68 +54,74 @@ static void cooling10Hz_PRD(void)
             break;
 
         case DRV_HSD_STATE_ON:
-            if (app_vehicleState_getState() != VEHICLESTATE_ON_HV)
+            if ((app_vehicleState_getState() != VEHICLESTATE_ON_HV) || (app_vehicleState_getState() != VEHICLESTATE_TS_RUN))
             {
                 drv_vn9008_setEnabled(DRV_VN9008_CHANNEL_PUMP, false);    // Power of pump controlled by the cooling manager
-                if (app_vehicleState_getState() != VEHICLESTATE_TS_RUN)
-                {
 #if FEATURE_IS_DISABLED(FEATURE_PUMP_FULL_BEANS)
-                    HW_TIM_setDuty(HW_TIM_PORT_PUMP, HW_TIM_CHANNEL_1, 0.00f);
+                HW_TIM_setDuty(HW_TIM_PORT_PUMP, HW_TIM_CHANNEL_1, 0.00f);
 #endif
-                }
             }
             break;
 
         case DRV_HSD_STATE_OVERCURRENT:
-            if ((app_vehicleState_getState() == VEHICLESTATE_TS_RUN) || (app_vehicleState_getState() == VEHICLESTATE_ON_HV))
-            {
-                drv_vn9008_setEnabled(DRV_VN9008_CHANNEL_PUMP, false);
-            }
-            else
-            {
+            drv_vn9008_setEnabled(DRV_VN9008_CHANNEL_PUMP, false);
 #if FEATURE_IS_DISABLED(FEATURE_PUMP_FULL_BEANS)
-                HW_TIM_setDuty(HW_TIM_PORT_PUMP, HW_TIM_CHANNEL_1, 0.00f);
+            HW_TIM_setDuty(HW_TIM_PORT_PUMP, HW_TIM_CHANNEL_1, 0.00f);
 #endif
-            }
             break;
 
         case DRV_HSD_STATE_OVERTEMP:
-            if (app_vehicleState_getState() != VEHICLESTATE_TS_RUN)
-            {
-                drv_vn9008_setEnabled(DRV_VN9008_CHANNEL_PUMP, false);
-            }
-            if (app_vehicleState_getState() != VEHICLESTATE_ON_HV)
-            {
+            drv_vn9008_setEnabled(DRV_VN9008_CHANNEL_PUMP, false);
 #if FEATURE_IS_DISABLED(FEATURE_PUMP_FULL_BEANS)
-                HW_TIM_setDuty(HW_TIM_PORT_PUMP, HW_TIM_CHANNEL_1, 0.00f);
+            HW_TIM_setDuty(HW_TIM_PORT_PUMP, HW_TIM_CHANNEL_1, 0.00f);
 #endif
-            }
             break;
 
         default:
             if ((app_vehicleState_getState() == VEHICLESTATE_TS_RUN) || (app_vehicleState_getState() == VEHICLESTATE_ON_HV))
             {
                 drv_vn9008_setEnabled(DRV_VN9008_CHANNEL_PUMP, false);
-            }
 #if FEATURE_IS_DISABLED(FEATURE_PUMP_FULL_BEANS)
-            HW_TIM_setDuty(HW_TIM_PORT_PUMP, HW_TIM_CHANNEL_1, 0.00f);
+                HW_TIM_setDuty(HW_TIM_PORT_PUMP, HW_TIM_CHANNEL_1, 0.00f);
 #endif
+            }
+
             break;
     }
-    else if (app_vehicleState_getState() == VEHICLESTATE_TS_RUN)
+
+    switch (fan_state)
     {
-        drv_vn9008_setEnabled(DRV_VN9008_CHANNEL_FAN, true);
-    }
-    else
-    {
-        drv_vn9008_setEnabled(DRV_VN9008_CHANNEL_FAN, false);
-        drv_vn9008_setEnabled(DRV_VN9008_CHANNEL_PUMP, false); // Power of pump controlled by the cooling manager
-#if FEATURE_IS_DISABLED(FEATURE_PUMP_FULL_BEANS)
-        HW_TIM_setDuty(HW_TIM_PORT_PUMP, HW_TIM_CHANNEL_1, 0.00f);
-#endif
+        case DRV_HSD_STATE_OFF:
+            if (app_vehicleState_getState() == VEHICLESTATE_TS_RUN)
+            {
+                drv_vn9008_setEnabled(DRV_VN9008_CHANNEL_FAN, true);
+            }
+            break;
+
+        case DRV_HSD_STATE_ON:
+            if (app_vehicleState_getState() != VEHICLESTATE_TS_RUN)
+            {
+                drv_vn9008_setEnabled(DRV_VN9008_CHANNEL_FAN, false);
+            }
+            break;
+
+        case DRV_HSD_STATE_OVERCURRENT:
+            drv_vn9008_setEnabled(DRV_VN9008_CHANNEL_FAN, false);
+            break;
+
+
+        case DRV_HSD_STATE_OVERTEMP:
+            drv_vn9008_setEnabled(DRV_VN9008_CHANNEL_FAN, false);
+            break;
+
+        default:
+            if (app_vehicleState_getState() != VEHICLESTATE_TS_RUN)
+            {
+                drv_vn9008_setEnabled(DRV_VN9008_CHANNEL_FAN, false);
+            }
+            break;
     }
 }
-
 /**
  * @brief  Cooling Module descriptor
  */
