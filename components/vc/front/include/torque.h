cdfdbe7e7cc198c659bee03a86d124132d229d2a
JoshLafleur <josh.lafleur@outlook.com>
2025-12-23 17:50:18 -0500

    torque manager: Implement basic traction control pid to reduce torque request

--- components/vc/front/include/torque.h
+++ components/vc/front/include/torque.h
@@ -27,15 +27,25 @@ typedef enum
 
 typedef enum
 {
-    LAUNCHCONTROL_INIT = 0x00U,
-    LAUNCHCONTROL_INACTIVE,
-    LAUNCHCONTROL_HOLDING,
-    LAUNCHCONTROL_SETTLING,
-    LAUNCHCONTROL_PRELOAD,
-    LAUNCHCONTROL_LAUNCH,
-    LAUNCHCONTROL_REJECTED,
-    LAUNCHCONTROL_ERROR,
-} torque_launchControl_E;
+    LC_STATE_INIT = 0x00U,
+    LC_STATE_INACTIVE,
+    LC_STATE_HOLDING,
+    LC_STATE_SETTLING,
+    LC_STATE_PRELOAD,
+    LC_STATE_LAUNCH,
+    LC_STATE_REJECTED,
+    LC_STATE_ERROR,
+} torque_launchControlState_E;
+
+typedef enum
+{
+    TC_STATE_INIT = 0x00U,
+    TC_STATE_INACTIVE,
+    TC_STATE_ACTIVE,
+    TC_STATE_FAULT_SENSOR,
+    TC_STATE_ERROR,
+    TC_STATE_LOCKOUT,
+} torque_tractionControlState_E;
 
 typedef enum
 {
@@ -53,15 +63,23 @@ typedef enum
  *            P U B L I C  F U N C T I O N  P R O T O T Y P E S
  ******************************************************************************/
 
-float32_t                torque_getTorqueRequest(void);
-float32_t                torque_getTorqueRequestMax(void);
-float32_t                torque_getTorqueDriverInput(void);
-torque_state_E           torque_getState(void);
-CAN_torqueManagerState_E torque_getStateCAN(void);
-torque_gear_E            torque_getGear(void);
-CAN_gear_E               torque_getGearCAN(void);
-torque_raceMode_E        torque_getRaceMode(void);
-CAN_raceMode_E           torque_getRaceModeCAN(void);
-torque_launchControl_E   torque_getLaunchControlState(void);
-CAN_launchControlState_E torque_getLaunchControlStateCAN(void);
-bool                     torque_isLaunching(void);
+float32_t                     torque_getTorqueRequest(void);
+float32_t                     torque_getTorqueRequestMax(void);
+float32_t                     torque_getTorqueRequestCorrection(void);
+float32_t                     torque_getTorqueDriverInput(void);
+float32_t                     torque_getSlipRaw(void);
+float32_t                     torque_getSlipErrorP(void);
+float32_t                     torque_getSlipErrorI(void);
+float32_t                     torque_getSlipErrorD(void);
+float32_t                     torque_getTorqueReduction(void);
+torque_state_E                torque_getState(void);
+CAN_torqueManagerState_E      torque_getStateCAN(void);
+torque_gear_E                 torque_getGear(void);
+CAN_gear_E                    torque_getGearCAN(void);
+torque_raceMode_E             torque_getRaceMode(void);
+CAN_raceMode_E                torque_getRaceModeCAN(void);
+torque_launchControlState_E   torque_getLaunchControlState(void);
+CAN_launchControlState_E      torque_getLaunchControlStateCAN(void);
+bool                          torque_isLaunching(void);
+torque_tractionControlState_E torque_getTractionControlState(void);
+CAN_tractionControlState_E    torque_getTractionControlStateCAN(void);
