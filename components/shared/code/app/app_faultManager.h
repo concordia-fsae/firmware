e40104aaf06852a40f24336248b025c8d127af1d
JoshLafleur <josh.lafleur@outlook.com>
2025-12-29 15:16:33 -0500

    faultManager: Bringup basic RX and fault status support

---
+++ components/shared/code/app/app_faultManager.h
@@ -0,0 +1,42 @@
+/**
+ * app_faultManager.h
+ * @brief Fault Manager header file
+ */
+
+#pragma once
+
+/******************************************************************************
+ *                             I N C L U D E S
+ ******************************************************************************/
+
+// The fault index and fault messages are generated from yamcan
+// TODO: Improve the generator
+#include "CANTypes_generated.h"
+#include "MessageUnpack_generated.h"
+
+#include "LIB_Types.h"
+#include "FeatureDefines_generated.h"
+
+/******************************************************************************
+ *                              D E F I N E S
+ ******************************************************************************/
+
+#define MAX_FAULTS 64U
+
+/******************************************************************************
+ *                               M A C R O S
+ ******************************************************************************/
+
+// Note: The following function must have a fault_index < 64U
+#define app_faultManager_getNetworkedFault_state(bus, msg, fault_index) \
+    (CANRX_get_rawMessage(bus, msg)->u32[fault_index / 32U] & (1U << (fault_index % 32U)))
+#define app_faultManager_getNetworkedFault_anySet(bus, msg) \
+    ((CANRX_get_rawMessage(bus, msg)->u32[0U] | CANRX_get_rawMessage(bus, msg)->u32[1U]) != 0U)
+
+/******************************************************************************
+ *            P U B L I C  F U N C T I O N  P R O T O T Y P E S
+ ******************************************************************************/
+
+void      app_faultManager_setFaultState(FM_fault_E fault, bool faulted);
+bool      app_faultManager_getFaultState(FM_fault_E fault);
+uint32_t* app_faultManager_transmit(void);
