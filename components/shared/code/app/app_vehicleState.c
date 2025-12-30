concordia-fsae/firmware

commit 9ce3b0220dd8d9e96e1b9a9259edc5e84547e783

vehicleState: Add sleep support

components/shared/code/app/app_vehicleState.c
@@ -19,6 +19,7 @@
 
 #define BOOT_TIME_MS 250U
 #define VEHICLESTATE_TIMEOUT_MS 1000U
+#define BOOT_SLEEP_DISABLE_MS 60000U
 
 /******************************************************************************
  *                         P R I V A T E  V A R S
@@ -27,7 +28,10 @@
 struct
 {
     app_vehicleState_state_E state;
-    drv_timer_S              timer;
+    drv_timer_S              bootTimer;
+
+    drv_timer_S                  sleepTimeout;
+    app_vehicleState_sleepable_E sleepState;
 } vehicleState_data;
 
 /******************************************************************************
@@ -54,7 +58,30 @@ static CAN_vehicleState_E translateToCANState(app_vehicleState_state_E state)
         case VEHICLESTATE_TS_RUN:
             ret = CAN_VEHICLESTATE_TS_RUN;
             break;
+        case VEHICLESTATE_SLEEP:
+            ret = CAN_VEHICLESTATE_SLEEP;
+            break;
         default:
+            break;
+    }
+
+    return ret;
+}
+
+static CAN_sleepFollowerState_E translateToCANSleepableState(app_vehicleState_sleepable_E state)
+{
+    CAN_sleepFollowerState_E ret = CAN_SLEEPFOLLOWERSTATE_SNA;
+
+    switch (state)
+    {
+        case SLEEPABLE_OK:
+            ret = CAN_SLEEPFOLLOWERSTATE_OK_TO_SLEEP;
+            break;
+        case SLEEPABLE_NOK:
+            ret = CAN_SLEEPFOLLOWERSTATE_NOK_TO_SLEEP;
+            break;
+        case SLEEPABLE_ALARM:
+            ret = CAN_SLEEPFOLLOWERSTATE_ALARM;
             break;
     }
 
@@ -73,9 +100,11 @@ void app_vehicleState_init(void)
     memset(&vehicleState_data, 0x00U, sizeof(vehicleState_data));
 
     vehicleState_data.state = VEHICLESTATE_INIT;
-    drv_timer_init(&vehicleState_data.timer);
+    drv_timer_init(&vehicleState_data.bootTimer);
+    drv_timer_init(&vehicleState_data.sleepTimeout);
+    drv_timer_start(&vehicleState_data.sleepTimeout, BOOT_SLEEP_DISABLE_MS);
 #if FEATURE_VEHICLESTATE_MODE == FDEFS_MODE_LEADER
-    drv_timer_start(&vehicleState_data.timer, BOOT_TIME_MS);
+    drv_timer_start(&vehicleState_data.bootTimer, BOOT_TIME_MS);
 #endif
 }
 
@@ -88,10 +117,10 @@ void app_vehicleState_run100Hz(void)
     switch (vehicleState_data.state)
     {
         case VEHICLESTATE_INIT:
-            if (drv_timer_getState(&vehicleState_data.timer) == DRV_TIMER_EXPIRED)
+            if (drv_timer_getState(&vehicleState_data.bootTimer) == DRV_TIMER_EXPIRED)
             {
                 vehicleState_data.state = VEHICLESTATE_ON_GLV;
-                drv_timer_stop(&vehicleState_data.timer);
+                drv_timer_stop(&vehicleState_data.bootTimer);
             }
             break;
         case VEHICLESTATE_ON_GLV:
@@ -118,7 +147,7 @@ void app_vehicleState_run100Hz(void)
                 {
                     vehicleState_data.state = VEHICLESTATE_TS_RUN;
                 }
-            }   
+            }
             break;
         case VEHICLESTATE_TS_RUN:
             if (drv_inputAD_getDigitalActiveState(VEHICLESTATE_INPUTAD_TSMS) == DRV_IO_INACTIVE)
@@ -126,13 +155,23 @@ void app_vehicleState_run100Hz(void)
                 vehicleState_data.state = VEHICLESTATE_ON_GLV;
             }
             break;
+        case VEHICLESTATE_SLEEP:
+            // Explicitly do nothing and handle later
+            break;
     }
-#else
+
+    if ((vehicleState_data.state != VEHICLESTATE_ON_GLV) &&
+        (vehicleState_data.state != VEHICLESTATE_INIT) &&
+        (vehicleState_data.state != VEHICLESTATE_SLEEP))
+    {
+        app_vehicleState_delaySleep(BOOT_SLEEP_DISABLE_MS);
+    }
+#else // FDEFS_MODE_LEADER
     CAN_vehicleState_E state = translateToCANState(vehicleState_data.state);
 
     if (VEHICLESTATE_CANRX_SIGNAL(&state) == CANRX_MESSAGE_VALID)
     {
-        drv_timer_stop(&vehicleState_data.timer);
+        drv_timer_stop(&vehicleState_data.bootTimer);
 
         switch (state)
         {
@@ -148,18 +187,45 @@ void app_vehicleState_run100Hz(void)
             case CAN_VEHICLESTATE_TS_RUN:
                 vehicleState_data.state = VEHICLESTATE_TS_RUN;
                 break;
+            case CAN_VEHICLESTATE_SLEEP:
+                vehicleState_data.state = VEHICLESTATE_SLEEP;
+                break;
         }
     }
-    else if (drv_timer_getState(&vehicleState_data.timer) == DRV_TIMER_STOPPED)
+    else if (drv_timer_getState(&vehicleState_data.bootTimer) == DRV_TIMER_STOPPED)
     {
-        drv_timer_start(&vehicleState_data.timer, VEHICLESTATE_TIMEOUT_MS);
+        drv_timer_start(&vehicleState_data.bootTimer, VEHICLESTATE_TIMEOUT_MS);
     }
-    else if (drv_timer_getState(&vehicleState_data.timer) == DRV_TIMER_EXPIRED)
+    else if (drv_timer_getState(&vehicleState_data.bootTimer) == DRV_TIMER_EXPIRED)
     {
         vehicleState_data.state = VEHICLESTATE_ON_GLV;
-        drv_timer_stop(&vehicleState_data.timer);
+        drv_timer_stop(&vehicleState_data.bootTimer);
     }
-#endif
+#endif // !FDEFS_MODE_LEADER
+
+    const bool sleepExpired = drv_timer_getState(&vehicleState_data.sleepTimeout) == DRV_TIMER_EXPIRED;
+    vehicleState_data.sleepState = sleepExpired ? SLEEPABLE_OK : SLEEPABLE_NOK;
+
+#if FEATURE_VEHICLESTATE_MODE == FDEFS_MODE_LEADER
+    if (sleepExpired)
+    {
+        CAN_sleepFollowerState_E swsSleepable = CAN_SLEEPFOLLOWERSTATE_SNA;
+        CAN_sleepFollowerState_E vcfrontSleepable = CAN_SLEEPFOLLOWERSTATE_SNA;
+        const bool canSleepSWS = (CANRX_get_signal(VEH, SWS_sleepable, &swsSleepable) == CANRX_MESSAGE_VALID) &&
+                                 (swsSleepable == CAN_SLEEPFOLLOWERSTATE_OK_TO_SLEEP);
+        const bool canSleepVCFRONT = (CANRX_get_signal(VEH, VCFRONT_sleepable, &vcfrontSleepable) == CANRX_MESSAGE_VALID) &&
+                                     (vcfrontSleepable == CAN_SLEEPFOLLOWERSTATE_OK_TO_SLEEP);
+
+        if (canSleepSWS && canSleepVCFRONT)
+        {
+            vehicleState_data.state = VEHICLESTATE_SLEEP;
+        }
+        else
+        {
+            app_vehicleState_init();
+        }
+    }
+#endif // FDEFS_MODE_LEADER
 }
 
 /**
@@ -169,6 +235,36 @@ void app_vehicleState_run100Hz(void)
 app_vehicleState_state_E app_vehicleState_getState(void)
 {
     return vehicleState_data.state;
+}
+
+/**
+ * @brief Delay this controller from allowing sleep for atleast the next amount of time
+ * @note If the timer is sleeping for more time than this already, do nothing
+ * @param ms The amount of ms to sleep for
+ */
+void app_vehicleState_delaySleep(uint32_t ms)
+{
+    const uint32_t timerEnd = drv_timer_getEndTimeMS(&vehicleState_data.sleepTimeout);
+    const uint32_t now = HW_TIM_getTimeMS();
+
+    if (timerEnd < (now + ms))
+    {
+        drv_timer_start(&vehicleState_data.sleepTimeout, ms);
+    }
+}
+
+bool app_vehicleState_sleeping(void)
+{
+    return app_vehicleState_getState() == VEHICLESTATE_SLEEP;
+}
+
+/**
+ * @brief Get current sleepable state of followers state
+ * @return the current sleepable state
+ */
+CAN_sleepFollowerState_E app_vehicleState_getSleepableStateCAN(void)
+{
+    return translateToCANSleepableState(vehicleState_data.sleepState);
 }
 
 #if FEATURE_VEHICLESTATE_MODE == FDEFS_MODE_LEADER

components/shared/code/app/app_vehicleState.h
@@ -81,7 +81,15 @@ typedef enum
     VEHICLESTATE_ON_GLV,
     VEHICLESTATE_ON_HV,
     VEHICLESTATE_TS_RUN,
+    VEHICLESTATE_SLEEP,
 } app_vehicleState_state_E;
+
+typedef enum
+{
+    SLEEPABLE_OK = 0x00U,
+    SLEEPABLE_NOK,
+    SLEEPABLE_ALARM,
+} app_vehicleState_sleepable_E;
 
 /******************************************************************************
  *            P U B L I C  F U N C T I O N  P R O T O T Y P E S
@@ -90,9 +98,11 @@ typedef enum
 // Functionality
 void app_vehicleState_init(void);
 void app_vehicleState_run100Hz(void);
-
+void app_vehicleState_delaySleep(uint32_t ms);
 // Accessors
+bool                     app_vehicleState_sleeping(void);
 app_vehicleState_state_E app_vehicleState_getState(void);
+CAN_sleepFollowerState_E app_vehicleState_getSleepableStateCAN(void);
 #if FEATURE_VEHICLESTATE_MODE == FDEFS_MODE_LEADER
 CAN_vehicleState_E       app_vehicleState_getStateCAN(void);
 #endif

commit f3e828fd67c3657057729cfeac7c637b558a1c6c

vehicleState: Fix sleep transition bug
Fixes: 9ce3b0220dd8d9e96e1b9a9259edc5e84547e783 "vehicleState: Add sleep
support"

components/shared/code/app/app_vehicleState.c
@@ -244,10 +244,10 @@ void app_vehicleState_run100Hz(void)
         {
             vehicleState_data.state = VEHICLESTATE_SLEEP;
         }
-        else
-        {
-            app_vehicleState_init();
-        }
+    }
+    else if (app_vehicleState_sleeping())
+    {
+        app_vehicleState_init();
     }
 #endif // FDEFS_MODE_LEADER
 }