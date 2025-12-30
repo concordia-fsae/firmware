2fee422cdc57d10f8beb28f3a8f71b21b5a678be
JoshLafleur <josh.lafleur@outlook.com>
2025-12-29 23:16:14 -0500

    powerManager: Improve thresholds, and clean up state machine logic

--- components/vc/pdu/src/powerManager.c
+++ components/vc/pdu/src/powerManager.c
@@ -18,8 +18,34 @@
 #include "drv_outputAD.h"
 #include "app_vehicleState.h"
 
+/******************************************************************************
+ *                              D E F I N E S
+ ******************************************************************************/
+
 #define PDU_CS_AMPS_PER_VOLT 0.20f
 #define PDU_VS_VOLTAGE_MULTIPLIER 3.61f
+
+// System design minimums
+// MINIMUM_TS_ACTIVE 9.0V
+// MINIMUM_BOARD     8.0V
+#define BATTERY_OVERVOLTAGE    15.0f
+#define BATTERY_RECHARGED      12.75f
+#define BATTERY_CUTOFF_SFTY_HI 12.75f
+#define BATTERY_CUTOFF_SFTY_LO 11.50f
+#define BATTERY_CUTOFF_LOAD_HI 11.25f
+#define BATTERY_CUTOFF_LOAD_LO 11.00f
+#define BATTERY_CUTOFF_ANY_HI  10.75f
+#define BATTERY_CUTOFF_ANY_LO  10.50f
+
+// Various power domain sanity checks
+_Static_assert(BATTERY_CUTOFF_ANY_LO < BATTERY_CUTOFF_ANY_HI, "Battery cutoff low must be lower than the high threshold.");
+_Static_assert(BATTERY_CUTOFF_LOAD_LO < BATTERY_CUTOFF_LOAD_HI, "Load cutoff low must be lower than the high threshold.");
+_Static_assert(BATTERY_CUTOFF_SFTY_LO < BATTERY_CUTOFF_SFTY_HI, "Safety cutoff low must be lower than the high threshold.");
+
+_Static_assert(BATTERY_CUTOFF_ANY_HI < BATTERY_CUTOFF_LOAD_LO, "Battery cutoff must be lower than the load cutoff.");
+_Static_assert(BATTERY_CUTOFF_LOAD_HI < BATTERY_CUTOFF_SFTY_LO, "Load cutoff must be lower than the safety cutoff.");
+_Static_assert(BATTERY_CUTOFF_SFTY_HI <= BATTERY_RECHARGED, "Battery must be considered recharged at a higher voltage than the highest cutoff.");
+_Static_assert(BATTERY_RECHARGED < BATTERY_OVERVOLTAGE, "Overvoltage must be highest voltage value.");
 
 /******************************************************************************
  *                         P R I V A T E  V A R S
@@ -33,7 +59,51 @@ static struct
         float32_t current;
         float32_t rail_5v_voltage;
     } pdu;
-} powerManager_data;
+
+    bool okBattery:1;
+    bool okLoads:1;
+    bool okSafety:1;
+
+    bool charged:1;
+    bool sleeping:1;
+    bool lowBattery:1;
+} pm_data;
+
+/******************************************************************************
+ *                     P R I V A T E  F U N C T I O N S
+ ******************************************************************************/
+
+static void getInputs(void)
+{
+    pm_data.glv_voltage = drv_inputAD_getAnalogVoltage(DRV_INPUTAD_ANALOG_UVL_BATT) * 6.62f;
+    pm_data.pdu.current = drv_inputAD_getAnalogVoltage(DRV_INPUTAD_ANALOG_DEMUX2_5V_SNS) * PDU_CS_AMPS_PER_VOLT;
+    pm_data.pdu.rail_5v_voltage = drv_inputAD_getAnalogVoltage(DRV_INPUTAD_ANALOG_5V_VOLTAGE) * PDU_VS_VOLTAGE_MULTIPLIER;
+    pm_data.sleeping = app_vehicleState_sleeping();
+}
+
+static void evalAbilities(void)
+{
+    const bool lowBattery = pm_data.glv_voltage < BATTERY_RECHARGED;
+    const bool charged = pm_data.charged ?
+                         pm_data.glv_voltage > BATTERY_CUTOFF_ANY_LO :
+                         pm_data.glv_voltage > BATTERY_RECHARGED;
+    bool okBattery = pm_data.okBattery ?
+                     pm_data.glv_voltage > BATTERY_CUTOFF_ANY_LO :
+                     pm_data.glv_voltage > BATTERY_CUTOFF_ANY_HI;
+    bool okLoads = pm_data.okLoads ?
+                   pm_data.glv_voltage > BATTERY_CUTOFF_LOAD_LO :
+                   pm_data.glv_voltage > BATTERY_CUTOFF_LOAD_HI;
+    bool okSafety = pm_data.okSafety ?
+                    pm_data.glv_voltage > BATTERY_CUTOFF_SFTY_LO :
+                    pm_data.glv_voltage > BATTERY_CUTOFF_SFTY_HI;
+    bool overvoltage = pm_data.glv_voltage > BATTERY_OVERVOLTAGE;
+
+    pm_data.lowBattery = lowBattery;
+    pm_data.charged = charged;
+    pm_data.okBattery = okBattery && !overvoltage;
+    pm_data.okLoads = okLoads && !pm_data.sleeping && !overvoltage && charged;
+    pm_data.okSafety = okSafety && !pm_data.sleeping && !overvoltage && charged;
+}
 
 /******************************************************************************
  *                       P U B L I C  F U N C T I O N S
@@ -41,17 +111,17 @@ static struct
 
 float32_t powerManager_getGLVVoltage(void)
 {
-    return powerManager_data.glv_voltage;
+    return pm_data.glv_voltage;
 }
 
 float32_t powerManager_getGLVCurrent(void)
 {
-    return powerManager_data.total_current;
+    return pm_data.total_current;
 }
 
 static void powerManager_init(void)
 {
-    memset(&powerManager_data, 0x00, sizeof(powerManager_data));
+    memset(&pm_data, 0x00, sizeof(pm_data));
 
     drv_tps2hb16ab_init();
     drv_vn9008_init();
@@ -70,19 +140,12 @@ static void powerManager_init(void)
 
 static void powerManager_periodic_100Hz(void)
 {
-    const float32_t glv_voltage = drv_inputAD_getAnalogVoltage(DRV_INPUTAD_ANALOG_UVL_BATT) * 6.62f;
-    const bool enable_loads = glv_voltage > 8.0f;
-    const bool enable_shutdown = glv_voltage > 9.0f;
-    const float32_t pdu_current = drv_inputAD_getAnalogVoltage(DRV_INPUTAD_ANALOG_DEMUX2_5V_SNS) * PDU_CS_AMPS_PER_VOLT;
-    const float32_t pdu_5v_voltage = drv_inputAD_getAnalogVoltage(DRV_INPUTAD_ANALOG_5V_VOLTAGE) * PDU_VS_VOLTAGE_MULTIPLIER;
-    const bool sleeping = app_vehicleState_sleeping();
-    powerManager_data.glv_voltage = glv_voltage;
-    powerManager_data.pdu.current = pdu_current;
-    powerManager_data.pdu.rail_5v_voltage = pdu_5v_voltage;
-
     float32_t tmp_current = 0.0f;
 
-    const drv_io_activeState_E shutdown_en = enable_shutdown ? DRV_IO_ACTIVE : DRV_IO_INACTIVE;
+    getInputs();
+    evalAbilities();
+
+    const drv_io_activeState_E shutdown_en = pm_data.okSafety ? DRV_IO_ACTIVE : DRV_IO_INACTIVE;
     drv_outputAD_setDigitalActiveState(DRV_OUTPUTAD_VCU_SFTY_EN, shutdown_en);
 
     // TODO: Improve
@@ -96,7 +159,7 @@ static void powerManager_periodic_100Hz(void)
             const bool requiredLoad = (((i == DRV_TPS2HB16AB_IC_VC1_VC2) && (n == DRV_TPS2HB16AB_OUT_1)) ||
                                        (i == DRV_TPS2HB16AB_IC_VCU1_VCU2) ||
                                        ((i == DRV_TPS2HB16AB_IC_HVE_COCKPIT) && (n == DRV_TPS2HB16AB_OUT_2)));
-            const bool enableLoad = enable_loads && (!sleeping || requiredLoad);
+            const bool enableLoad = pm_data.okLoads || (requiredLoad && pm_data.okBattery);
 
             drv_hsd_state_E state = drv_tps2hb16ab_getState(i, n);
             if (state == DRV_HSD_STATE_OVERCURRENT)
@@ -126,7 +189,7 @@ static void powerManager_periodic_100Hz(void)
         tmp_current += drv_vn9008_getCurrent(i);
     }
 
-    powerManager_data.total_current = tmp_current + pdu_current;
+    pm_data.total_current = tmp_current + pm_data.pdu.current;
 
     output = (output + 1) % DRV_TPS2HB16AB_OUT_COUNT;
     drv_tps2hb16ab_setCSChannel(DRV_TPS2HB16AB_IC_BMS1_SHUTDOWN, output); // All CS select pins are on the same GPIO
@@ -134,12 +197,12 @@ static void powerManager_periodic_100Hz(void)
 
 float32_t powerManager_getPduCurrent(void)
 {
-    return powerManager_data.pdu.current;
+    return pm_data.pdu.current;
 }
 
 float32_t powerManager_getPdu5vVoltage(void)
 {
-    return powerManager_data.pdu.rail_5v_voltage;
+    return pm_data.pdu.rail_5v_voltage;
 }
 
 /******************************************************************************
