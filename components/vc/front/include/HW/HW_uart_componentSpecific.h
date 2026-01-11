12f759bc2e1e1cf88d17501a1f227a7ab342ff0b
JoshLafleur <josh.lafleur@outlook.com>
2026-01-10 21:35:34 -0500

    vcfront: Bringup uart driver

---
+++ components/vc/front/include/HW/HW_uart_componentSpecific.h
@@ -0,0 +1,16 @@
+/**
+ * @file HW_uart_componentSpecific.h
+ * @brief  Header file for component specific UART firmware
+ */
+
+#pragma once
+
+/******************************************************************************
+ *                             T Y P E D E F S
+ ******************************************************************************/
+
+typedef enum
+{
+    HW_UART_PORT_GPS = 0x00U, // UART3
+    HW_UART_PORT_COUNT,
+} HW_UART_port_E;
