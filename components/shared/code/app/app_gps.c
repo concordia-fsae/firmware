3a9fbe1ce2d76ebc51154380f6cf8969f001e68c
JoshLafleur <josh.lafleur@outlook.com>
2026-01-10 23:32:24 -0500

    gps: Buffer overrun detection

--- components/shared/code/app/app_gps.c
+++ components/shared/code/app/app_gps.c
@@ -165,19 +165,32 @@ static void app_gps_init(void)
 static void app_gps_periodic_100Hz(void)
 {
 #if FEATURE_IS_ENABLED(FEATURE_GPSTRANSCEIVER)
-    // TODO: Overrun detection
+    bool overrun = false;
+
     while (LIB_BUFFER_CIRC_PEEK(&gps.dmaBuffer))
     {
-        uint8_t ch = LIB_BUFFER_CIRC_GETSET(&gps.dmaBuffer, 0U);
-        LIB_BUFFER_FIFO_INSERT(&gps.sentence, ch);
-        if (ch == '\n')
+        if (LIB_BUFFER_CIRC_PEEKN(&gps.dmaBuffer, -1))
         {
-            parse((uint8_t*)&gps.sentence.buffer, LIB_BUFFER_FIFO_GETLENGTH(&gps.sentence));
-            LIB_BUFFER_FIFO_CLEAR(&gps.sentence);
+            HW_UART_stopDMA(HW_UART_PORT_GPS);
+            LIB_BUFFER_CIRC_CLEAR(&gps.dmaBuffer);
+            HW_UART_startDMARX(HW_UART_PORT_GPS, (uint32_t*)&gps.dmaBuffer, BUFFER_SIZE);
+            overrun = true;
+        }
+        else
+        {
+            uint8_t ch = LIB_BUFFER_CIRC_GETSET(&gps.dmaBuffer, 0U);
+            LIB_BUFFER_FIFO_INSERT(&gps.sentence, ch);
+            if (ch == '\n')
+            {
+                parse((uint8_t*)&gps.sentence.buffer, LIB_BUFFER_FIFO_GETLENGTH(&gps.sentence));
+                LIB_BUFFER_FIFO_CLEAR(&gps.sentence);
+            }
         }
     }
 
-    app_faultManager_setFaultState(GPS_DEVICE_ERROR, app_gps_isValid());
+    const bool gpsValid = app_gps_isValid();
+
+    app_faultManager_setFaultState(GPS_DEVICE_ERROR, gpsValid || overrun);
 #else // FEATURE_GPSTRANSCEIVER
     // TODO: Implement GPS listener
 #endif // !FEATURE_GPSTRANSCEIVER
