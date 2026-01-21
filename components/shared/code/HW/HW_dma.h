4033d25d333cbafac8ab1b1963ac764c65c25fa6
JoshLafleur <josh.lafleur@outlook.com>
2026-01-20 19:58:34 -0500

    dma: Add more firmware definitions

--- components/shared/code/HW/HW_dma.h
+++ components/shared/code/HW/HW_dma.h
@@ -22,6 +22,28 @@ typedef enum
     BUFFER_HALF_UPPER,
 } HW_dma_bufferHalf_E;
 
+typedef enum
+{
+    HW_DMA_1 = 0x00U,
+    HW_DMA_2,
+    HW_DMA_COUNT,
+} HW_dma_instance_E;
+
+typedef enum
+{
+    HW_DMA_CHANNEL_NONE = 0x00U,
+    HW_DMA_CHANNEL_1,
+    HW_DMA_CHANNEL_2,
+    HW_DMA_CHANNEL_3,
+    HW_DMA_CHANNEL_4,
+    HW_DMA_CHANNEL_5,
+    HW_DMA_CHANNEL_6,
+    HW_DMA_CHANNEL_7,
+    HW_DMA_CHANNEL_8,
+} HW_dma_channel_E;
+#define HW_DMA1_CHANNEL_COUNT HW_DMA_CHANNEL_8
+#define HW_DMA2_CHANNEL_COUNT HW_DMA_CHANNEL_5
+
 /******************************************************************************
  *            P U B L I C  F U N C T I O N  P R O T O T Y P E S
  ******************************************************************************/
