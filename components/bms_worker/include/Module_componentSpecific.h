d597fc544bace84274537633e3e86ab1e1863d74
Josh Lafleur <josh.lafleur@flyzipline.com>
2026-03-15 21:11:14 -0400

    bmsw: Refactor to use shared RTOS library

---
+++ components/bms_worker/include/Module_componentSpecific.h
@@ -0,0 +1,44 @@
+/**
+ * @file Module_componentSpecific.h
+ * @brief  Header file for Module Manager for the component specific modules
+ */
+
+#pragma once
+
+/******************************************************************************
+ *                             I N C L U D E S
+ ******************************************************************************/
+
+// System Includes
+#include "ModuleDesc.h"
+
+/******************************************************************************
+ *                              E X T E R N S
+ ******************************************************************************/
+
+/**< Modules */
+extern const ModuleDesc_S CANIO_rx;
+extern const ModuleDesc_S UDS_desc;
+extern const ModuleDesc_S drv_inputAD_desc;
+extern const ModuleDesc_S BMS_desc;
+extern const ModuleDesc_S ENV_desc;
+extern const ModuleDesc_S cooling_desc;
+extern const ModuleDesc_S SYS_desc;
+extern const ModuleDesc_S CANIO_tx;
+
+/******************************************************************************
+ *                             T Y P E D E F S
+ ******************************************************************************/
+
+typedef enum
+{
+    MODULE_CANIO_rx = 0x00U,
+    MODULE_UDS,
+    MODULE_INPUTAD,
+    MODULE_BMS,
+    MODULE_ENV,
+    MODULE_COOLING,
+    MODULE_SYS,
+    MODULE_CANIO_tx,
+    MODULE_CNT
+} Module_tasks_E;
