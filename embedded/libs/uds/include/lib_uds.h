ef70a4f52a3aa35367403d5e187f3bc1cc3ad7ea
JoshLafleur <josh.lafleur@outlook.com>
2026-04-09 15:45:06 -0400

    uds: Support common DID's

--- embedded/libs/uds/include/lib_uds.h
+++ embedded/libs/uds/include/lib_uds.h
@@ -21,6 +21,7 @@
 
 #define UDS_SUB_FUNCTION_NONE                 0x00
 #define UDS_RESPONSE_NOT_REQUIRED             0x80
+#define UDS_DID_CURRENT_SESSION               0x0102
 
 #define uds_sendPositiveResponseEmpty(sid)    uds_sendPositiveResponse(sid, 0x00, NULL, 0U)
 
@@ -221,6 +222,7 @@ uint32_t         udsSrv_timeSinceLastTp(void);
 udsResult_E uds_sendNegativeResponse(udsServiceId_E sid, udsNegativeResponse_E nrc);
 udsResult_E uds_sendPositiveResponse(udsServiceId_E sid, uint8_t subFunction, uint8_t *payload, uint8_t payloadLengthBytes);
 bool        uds_checkResponseRequired(udsRequestDesc_S *req);
+bool        uds_handleCommonDIDRead(uint16_t did);
 
 
 /******************************************************************************
